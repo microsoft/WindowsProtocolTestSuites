3dda8d1d64104d98c42651714492ef5dd5b0d1a2
Yan Zeng <yazeng@microsoft.com>
2016-01-13 13:37:48 +0800

    Update RefreshTestConfig.cmd

--- TestSuites/FileServer/src/Batch/RefreshTestConfig.cmd
+++ TestSuites/FileServer/src/Batch/RefreshTestConfig.cmd
@@ -16,13 +16,12 @@ if not defined vspath (
 	)
 )
 
-echo:
 echo Define the command to run file sharing test suite
+
 if "%USERDOMAIN%" == "%COMPUTERNAME%" (
     REM workgroup environment
-    set RunFileSharingTestSuite="%vspath%..\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "..\Bin\MS-DFSC_ServerTestSuite.dll" "..\Bin\MS-SMB2_ServerTestSuite.dll" "..\Bin\MS-SMB2Model_ServerTestSuite.dll" "..\Bin\MS-FSA_ServerTestSuite.dll" /Settings:..\Bin\ServerLocalTestRun.testrunconfig /Logger:trx
+    set RunFileSharingTestSuite=%vspath%"..\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" ..\Bin\MS-DFSC_ServerTestSuite.dll ..\Bin\MS-SMB2_ServerTestSuite.dll ..\Bin\MS-SMB2Model_ServerTestSuite.dll ..\Bin\MS-FSA_ServerTestSuite.dll /Settings:..\Bin\ServerLocalTestRun.testrunconfig /Logger:trx
 ) else (
     REM domain environment
-    set RunFileSharingTestSuite="%vspath%..\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "..\Bin\ServerFailoverTestSuite.dll" "..\Bin\MS-SMB2Model_ServerTestSuite.dll" "..\Bin\MS-FSRVP_ServerTestSuite.dll" "..\Bin\MS-DFSC_ServerTestSuite.dll" "..\Bin\MS-SMB2_ServerTestSuite.dll" "..\Bin\MS-RSVD_ServerTestSuite.dll" "..\Bin\MS-SQOS_ServerTestSuite.dll" "..\Bin\Auth_ServerTestSuite.dll" "..\Bin\MS-FSA_ServerTestSuite.dll" /Settings:..\Bin\ServerLocalTestRun.testrunconfig /Logger:trx
+    set RunFileSharingTestSuite=%vspath%"..\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "..\Bin\ServerFailoverTestSuite.dll" "..\Bin\MS-SMB2Model_ServerTestSuite.dll" "..\Bin\MS-FSRVP_ServerTestSuite.dll" "..\Bin\MS-DFSC_ServerTestSuite.dll" "..\Bin\MS-SMB2_ServerTestSuite.dll" "..\Bin\MS-RSVD_ServerTestSuite.dll" "..\Bin\MS-SQOS_ServerTestSuite.dll" "..\Bin\Auth_ServerTestSuite.dll" "..\Bin\MS-FSA_ServerTestSuite.dll" /Settings:..\Bin\ServerLocalTestRun.testrunconfig /Logger:trx
 )
-
