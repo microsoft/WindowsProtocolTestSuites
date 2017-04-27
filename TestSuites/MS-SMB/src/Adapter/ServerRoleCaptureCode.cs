44be8b78c9fd0ae8d0729adbd79c95230e27c70e
Vick Ding <v-wedin@microsoft.com>
2017-04-27 10:06:06 +0800

    Remove TDI and TDQ from config or testsuite files

--- TestSuites/MS-SMB/src/Adapter/ServerRoleCaptureCode.cs
+++ TestSuites/MS-SMB/src/Adapter/ServerRoleCaptureCode.cs
@@ -1279,10 +1279,6 @@ namespace Microsoft.Protocol.TestSuites.Smb
 
             //
             // Verify MS-SMB requirement: MS-SMB_R9219.
-            //
-            // Based on the latest TD v20101101 to the fixed TDI #53077, the description for RS MS-SMB_R9219 has been changed to as below:
-            // "ChallengeLength (1 byte): The value of this field MUST be 0x08 and is the length of the random challenge used in challenge/response authentication."
-            // Currently only change the capture logic according to MIP's rule.
             bool isVerifyR9219 = ((1 == Marshal.SizeOf(response.SmbParameters.ChallengeLength))
                 && (0x08 == response.SmbParameters.ChallengeLength));
 
