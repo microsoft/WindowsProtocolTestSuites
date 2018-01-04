c5e10b6532384bbc13c4895e5f0d7c7e1968f494
FAREAST\vtian <vtian@microsoft.com>
2018-01-04 16:52:00 +0800

    Fix Bug #12665180: update batch files of RDP client test suite. 1: update the commonruntestcase.cmd file to be more reusable, flexible and robust. 2: Remove the case by case batch file

--- TestSuites/RDP/Client/src/Batch/Rdpeudp_RunAllTestCases.cmd
+++ TestSuites/RDP/Client/src/Batch/Rdpeudp_RunAllTestCases.cmd
@@ -1,5 +1,7 @@
 :: Copyright (c) Microsoft. All rights reserved.
 :: Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-.\CommonRunTestCase.cmd  "..\Bin\RDP_ClientTestSuite.dll" /Settings:..\Bin\ClientLocal.TestSettings /TestCaseFilter:"TestCategory=RDPEUDP" 
+call CommonRunTestCase.cmd  "TestCategory=RDPEUDP"
+%RunRDPTestSuite%
+
 pause
