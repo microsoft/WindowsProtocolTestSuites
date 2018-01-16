425dccb1394a8374144b3090d1fb3968754e6e0d
Michael Tsai <hstsai@microsoft.com>
2018-01-16 17:34:14 +0800

    Create a tool to generate TestCategories.xml

---
+++ ProtocolTestManager/Plugins/FileServerPlugin/FileServerToolForModelCases/Program.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Protocols.TestManager.FileServerToolForModelCases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppConfig config = AppConfig.LoadConfig();
+            TestSuite testSuite = new TestSuite();
+            testSuite.LoadFrom(config.TestSuiteAssembly);
+            testSuite.AppendCategoryForFileServerModelCases();
+        }
+    }
+}
