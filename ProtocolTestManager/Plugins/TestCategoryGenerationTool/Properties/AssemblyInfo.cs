9afb17fe44f2062857fdf7824a660f05d8794057
Michael Tsai <hstsai@microsoft.com>
2018-01-23 11:11:19 +0800

    Remove unused information in AssemblyInfo

--- ProtocolTestManager/Plugins/TestCategoryGenerationTool/Properties/AssemblyInfo.cs
+++ ProtocolTestManager/Plugins/TestCategoryGenerationTool/Properties/AssemblyInfo.cs
@@ -2,25 +2,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Reflection;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("TestCategoryGenerationTool")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("TestCategoryGenerationTool")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("3.18.3.0")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d217cb9b-c712-49fc-b135-4742a625bb94")]
