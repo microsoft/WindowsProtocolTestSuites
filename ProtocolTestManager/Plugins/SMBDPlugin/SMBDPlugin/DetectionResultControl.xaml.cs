38eaac567642a10ecea8d9744818cce1a050b01c
Huiren Jiang <hujia@microsoft.com>
2018-04-12 17:16:06 +0800

    fix

---
+++ ProtocolTestManager/Plugins/SMBDPlugin/SMBDPlugin/DetectionResultControl.xaml.cs
@@ -0,0 +1,279 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Microsoft.Protocols.TestTools.StackSdk.FileAccessService.Smb2;
+using Microsoft.Protocols.TestTools.StackSdk.FileAccessService.Rsvd;
+using Microsoft.Protocols.TestTools.StackSdk.FileAccessService.Sqos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Microsoft.Protocols.TestManager.SMBDPlugin
+{
+
+    public partial class DetectionResultControl : UserControl
+    {
+        public DetectionResultControl()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadDetectionInfo(DetectionInfo detectionInfo)
+        {
+            this.info = detectionInfo;
+
+            ResultMapList.ItemsSource = resultItemMapList;
+        }
+
+        #region Properties
+
+        private DetectionInfo info = null;
+
+        private const string dialectsDescription = "\"Max Supported Dialect\" is the selected one in the Negotiate Response by server when a Negotiate Request is sent to SUT with Dialects Smb2.002, Smb2.1, Smb3.0 and Smb3.02.";
+        private const string capabilitiesDescription = "\"Capabilities\" are found supported or not supported by analyzing the flags set in Negotiate Response when a Negotiate Request is sent to SUT with all defined flags in TD set in Capabilities field.";
+        private const string ioctlCodesDescription = "\"IoCtl Codes\" are found supported or not supported by analyzing IOCTL Responses when the following IOCTL Requests are sent to SUT.";
+        private const string createContextsDescription = "\"Creat Contexts\" are found supported or not supported by analyzing Create Responses when the Create Requests with the following create contexts are sent to SUT.";
+        private const string rsvdDescription = "\"RSVD Implementation\" is detected by sending Create Request with SVHDX_OPEN_DEVICE_CONTEXT\\SVHDX_OPEN_DEVICE_CONTEXT_V2.";
+        private const string sqosDescription = "\"SQOS Implementation\" is detected by sending SQOS get status request.";
+
+
+        private ResultItemMap dialectsItems = new ResultItemMap() { Header = "Max Smb Version Supported", Description = dialectsDescription };
+        private ResultItemMap capabilitiesItems = new ResultItemMap() { Header = "Capabilities", Description = capabilitiesDescription };
+        private ResultItemMap ioctlCodesItems = new ResultItemMap() { Header = "IoCtl Codes", Description = ioctlCodesDescription };
+        private ResultItemMap createContextsItems = new ResultItemMap() { Header = "Create Contexts", Description = createContextsDescription };
+
+        private ResultItemMap rsvdItems = new ResultItemMap() { Header = "Remote Shared Virtual Disk (RSVD)", Description = rsvdDescription };
+        private ResultItemMap sqosItems = new ResultItemMap() { Header = "Storage Quality of Service (SQOS)", Description = sqosDescription };
+
+        private List<ResultItemMap> resultItemMapList = new List<ResultItemMap>();
+
+        #endregion
+
+        #region Private functions
+
+  
+        private void AddResultItem(ref ResultItemMap resultItemMap, string value, DetectResult result)
+        {
+            string imagePath = string.Empty;
+            switch (result)
+            {
+                case DetectResult.Supported:
+                    imagePath = "/SMBDPlugin;component/Icons/supported.png";
+                    break;
+                case DetectResult.UnSupported:
+                    imagePath = "/SMBDPlugin;component/Icons/unsupported.png";
+                    break;
+                case DetectResult.DetectFail:
+                    imagePath = "/SMBDPlugin;component/Icons/undetected.png";
+                    break;
+                default:
+                    break;
+            }
+
+            ResultItem item = new ResultItem() { DetectedResult = result, ImageUrl = imagePath, Name = value };
+            resultItemMap.ResultItemList.Add(item);
+        }
+
+        private void AddRSVD(DetectionInfo info)
+        {
+            if (info.RsvdSupport == DetectResult.Supported)
+            {
+                if (info.RsvdVersion == RSVD_PROTOCOL_VERSION.RSVD_PROTOCOL_VERSION_1)
+                {
+                    AddResultItem(ref this.rsvdItems, "RSVD Protocol version 1", DetectResult.Supported);
+                    AddResultItem(ref this.rsvdItems, "RSVD Protocol version 2", DetectResult.UnSupported);
+                }
+                else
+                {
+                    AddResultItem(ref this.rsvdItems, "RSVD Protocol version 1", DetectResult.Supported);
+                    AddResultItem(ref this.rsvdItems, "RSVD Protocol version 2", DetectResult.Supported);
+                }
+            }
+            else if (info.RsvdSupport == DetectResult.UnSupported)
+            {
+                AddResultItem(ref this.rsvdItems, "The server doesn't support RSVD", info.RsvdSupport);
+            }
+            else
+            {
+                AddResultItem(ref this.rsvdItems, "Detection failed", this.info.RsvdSupport);
+            }
+        }
+
+        private void AddSQOS(DetectionInfo info)
+        {
+            if (info.SqosSupport == DetectResult.Supported)
+            {
+                if (info.SqosVersion == SQOS_PROTOCOL_VERSION.Sqos10)
+                {
+                    AddResultItem(ref this.sqosItems, "SQOS dialect 1.0", DetectResult.Supported);
+                    AddResultItem(ref this.sqosItems, "SQOS dialect 1.1", DetectResult.UnSupported);
+                }
+                else
+                {
+                    AddResultItem(ref this.sqosItems, "SQOS dialect 1.0", DetectResult.Supported);
+                    AddResultItem(ref this.sqosItems, "SQOS dialect 1.1", DetectResult.Supported);
+                }
+            }
+            else if (info.SqosSupport == DetectResult.UnSupported)
+            {
+                AddResultItem(ref this.sqosItems, "The server doesn't support SQOS", info.SqosSupport);
+            }
+            else
+            {
+                AddResultItem(ref this.sqosItems, "Detection failed", this.info.SqosSupport);
+            }
+        }
+        #endregion
+
+        #region Private events
+
+        private void MapSelectedChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox selectedMap = sender as ListBox;
+
+            //Keep all map headers unselected
+            selectedMap.UnselectAll();
+        }
+
+        private void ItemSelectedChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox selectedList = sender as ListBox;
+
+            if (selectedList.SelectedIndex != -1)
+            {
+                SetOtherItemsUnselected(sender);
+
+                ResultItem tempItem = (ResultItem)selectedList.SelectedItem;
+
+                if (tempItem.DetectedResult == DetectResult.UnSupported)
+                {
+                    this.ItemDescription.Text = tempItem.Name + " is found not supported after detection";
+                    return;
+                }
+                if (!info.detectExceptions.ContainsKey(tempItem.Name))
+                {
+                    this.ItemDescription.Text = tempItem.Name + " is found supported after detection";
+                    return;
+                }
+                string log = info.detectExceptions[tempItem.Name];
+                if (!string.IsNullOrEmpty(log))
+                {
+                    this.ItemDescription.Text = log;
+                }
+            }
+        }
+
+        private void SetOtherItemsUnselected(object sender)
+        {
+            ListBox selectedList = sender as ListBox;
+            ResultItem selectedItem = (ResultItem)selectedList.SelectedItem;
+
+            foreach (object obj in this.ResultMapList.Items)
+            {
+                //Find the controls in the DataTemplate with the help of VisualTreeHelper class
+                ListBoxItem lbi = this.ResultMapList.ItemContainerGenerator.ContainerFromItem(obj) as ListBoxItem;
+                ContentPresenter tempContentPresenter = FindVisualChild<ContentPresenter>(lbi);
+                if (tempContentPresenter != null)
+                {
+                    DataTemplate tempDataTemplate = tempContentPresenter.ContentTemplate;
+                    Expander mapHeader = tempDataTemplate.FindName("ResultMapHeader", tempContentPresenter) as Expander;
+                    ListBox itemList = tempDataTemplate.FindName("ResultItemList", tempContentPresenter) as ListBox;
+                    
+                    //Keep the current selection
+                    if (!itemList.Items.Contains(selectedItem))
+                        itemList.UnselectAll();
+                }
+            }
+        }
+
+        private void ResultMapHeader_Collapsed(object sender, RoutedEventArgs e)
+        {
+            Expander expander = sender as Expander;
+
+            foreach (object obj in this.ResultMapList.Items)
+            {
+                ListBoxItem lbi = this.ResultMapList.ItemContainerGenerator.ContainerFromItem(obj) as ListBoxItem;
+                ContentPresenter tempContentPresenter = FindVisualChild<ContentPresenter>(lbi);
+                if (tempContentPresenter != null)
+                {
+                    DataTemplate tempDataTemplate = tempContentPresenter.ContentTemplate;
+                    Expander mapHeader = tempDataTemplate.FindName("ResultMapHeader", tempContentPresenter) as Expander;
+                    ListBox itemList = tempDataTemplate.FindName("ResultItemList", tempContentPresenter) as ListBox;
+
+                    //Find the target list and clear the selection
+                    if (expander.Header == mapHeader.Header)
+                    {
+                        if (itemList.SelectedIndex != -1)
+                            this.ItemDescription.Text = string.Empty;
+
+                        itemList.UnselectAll();
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void ResultMapHeader_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Expander mapHeader = sender as Expander;
+            foreach (ResultItemMap map in resultItemMapList)
+            {
+                if (map.Header == mapHeader.Header.ToString())
+                {
+                    this.ItemDescription.Visibility = System.Windows.Visibility.Collapsed;
+                    this.MapDescription.Visibility = System.Windows.Visibility.Visible;
+                    this.MapDescription.Text = map.Description;
+                }
+            }
+        }
+
+        private void ResultMapHeader_MouseLeave(object sender, MouseEventArgs e)
+        {
+            this.MapDescription.Visibility = System.Windows.Visibility.Collapsed;
+            this.ItemDescription.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void ResultMapList_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            this.MapDescription.Visibility = System.Windows.Visibility.Collapsed;
+            this.ItemDescription.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private childItem FindVisualChild<childItem>(DependencyObject obj) where childItem : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is childItem)
+                    return (childItem)child;
+                else
+                {
+                    childItem childOfChild = FindVisualChild<childItem>(child);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+            return null;
+        }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer scv = sender as ScrollViewer;
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
+
+        #endregion
+
+    }
+}
---
+++ ProtocolTestManager/Plugins/SMBDPlugin/SMBDPlugin/SMBDValueDetector.cs
@@ -0,0 +1,144 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using Microsoft.Protocols.TestTools.StackSdk.Security.Sspi;
+using Microsoft.Protocols.TestTools.StackSdk.FileAccessService.Smb2;
+using Microsoft.Protocols.TestTools.StackSdk.FileAccessService.Rsvd;
+using System.Net;
+using System.Reflection;
+using System.Net.NetworkInformation;
+using Microsoft.Protocols.TestManager.SMBDPlugin;
+using System.Windows;
+using System.ComponentModel;
+
+namespace Microsoft.Protocols.TestManager.Detector
+{
+    public class SMBDValueDetector : IValueDetector
+    {
+        private DetectionInfo detectionInfo = new DetectionInfo();
+
+        /// <summary>
+        /// return false if check failed and set failed property in dictionary
+        /// </summary>
+        /// <param name="properties"></param>
+        /// <returns></returns>
+        public bool CheckConfigrationSettings(Dictionary<string, string> properties)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or
+        /// resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+        }
+
+        public bool GetDetectedProperty(out Dictionary<string, List<string>> propertiesDic)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Adds Detection steps to the log shown when detecting
+        /// </summary>
+        public List<DetectingItem> GetDetectionSteps()
+        {
+            List<DetectingItem> DetectingItems = new List<DetectingItem>();
+            DetectingItems.Add(new DetectingItem("Ping Target SUT", DetectingStatus.Pending, LogStyle.Default));
+            DetectingItems.Add(new DetectingItem("Check Credential", DetectingStatus.Pending, LogStyle.Default));
+            DetectingItems.Add(new DetectingItem("Get OS Version", DetectingStatus.Pending, LogStyle.Default));
+            DetectingItems.Add(new DetectingItem("Connect to share(Non-RDMA)", DetectingStatus.Pending, LogStyle.Default));
+            DetectingItems.Add(new DetectingItem("Connect to share(RDMA)", DetectingStatus.Pending, LogStyle.Default));
+            DetectingItems.Add(new DetectingItem("Check RDMA capability", DetectingStatus.Pending, LogStyle.Default));
+            return DetectingItems;
+        }
+
+        /// <summary>
+        /// Gets the list of properties that will be hidden in the configure page.
+        /// </summary>
+        /// <param name="rules">Selected rules.</param>
+        /// <returns>The list of properties which will not be shown in the configure page.</returns>
+        public List<string> GetHiddenProperties(List<CaseSelectRule> rules)
+        {
+            var hiddenProperties = new List<string>();
+            return hiddenProperties;
+        }
+
+        /// <summary>
+        /// Get the prerequisites for auto-detection.
+        /// </summary>
+        /// <returns>A instance of Prerequisites class.</returns>
+        public Prerequisites GetPrerequisites()
+        {
+            var prerequisites = new Prerequisites();
+            prerequisites.Title = "MS-SMBD";
+            prerequisites.Summary = "Please input the below info to detect SUT.\r\nIf SUT is in WORKGROUP, leave Domain Name blank.";
+
+            // Properties
+            prerequisites.Properties = new Dictionary<string, List<string>>();
+
+            string sutName = DetectorUtil.GetPropertyValue("SutComputerName");
+            prerequisites.Properties.Add("SUT Name", new List<string> { sutName });
+
+            string serverNonRNicIp = DetectorUtil.GetPropertyValue("ServerNonRNicIp");
+            prerequisites.Properties.Add("Server Non-RDMA NIC IP Address", new List<string> { serverNonRNicIp });
+
+            string serverRNicIp = DetectorUtil.GetPropertyValue("ServerRNicIp");
+            prerequisites.Properties.Add("Server RDMA NIC IP Address", new List<string> { serverRNicIp });
+
+            string sutUserName = DetectorUtil.GetPropertyValue("SutUserName");
+            prerequisites.Properties.Add("SUT User Name", new List<string> { sutUserName });
+
+            string sutPassword = DetectorUtil.GetPropertyValue("SutPassword");
+            prerequisites.Properties.Add("SUT Password", new List<string> { sutPassword });
+
+            string shareFolder = DetectorUtil.GetPropertyValue("ShareFolder");
+            prerequisites.Properties.Add("SUT Shared Folder", new List<string> { shareFolder });
+
+            return prerequisites;
+        }
+
+        public List<CaseSelectRule> GetSelectedRules()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets a summary of the detect result.
+        /// </summary>
+        /// <returns>Detect result.</returns>
+        public object GetSUTSummary()
+        {
+            DetectionResultControl SUTSummaryControl = new DetectionResultControl();
+            SUTSummaryControl.LoadDetectionInfo(detectionInfo);
+            return SUTSummaryControl;
+        }
+
+        /// <summary>
+        /// Runs property auto detection.
+        /// </summary>
+        /// <returns>Return true if the function is succeeded.</returns>
+        public bool RunDetection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SelectEnvironment(string NetworkEnvironment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetPrerequisiteProperties(Dictionary<string, string> properties)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
