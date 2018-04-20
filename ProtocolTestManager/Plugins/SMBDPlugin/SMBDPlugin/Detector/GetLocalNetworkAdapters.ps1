294d7b51a3e9bcde19de989928d9fda3742da462
Huiren Jiang <hujia@microsoft.com>
2018-04-20 09:02:50 +0800

    fix

--- ProtocolTestManager/Plugins/SMBDPlugin/SMBDPlugin/Detector/GetLocalNetworkAdapters.ps1
+++ ProtocolTestManager/Plugins/SMBDPlugin/SMBDPlugin/Detector/GetLocalNetworkAdapters.ps1
@@ -4,11 +4,20 @@
 # This script is used to test the credential of SUT computer
 # Return Value: 0/-1 indicates OK/NG
 
+$results = @()
 $adapters = Get-NetAdapter
 foreach($adapter in $adapters) {
 	if($adapter.Status -ne "Up") {
 		continue
 	}
 	$ipSettings = Get-NetIPAddress -ifIndex $adapter.ifIndex | Where-Object { $_.AddressFamily -eq "IPv4" }
-	if($ip)
+	if($ipSettings -eq $null){
+		continue
+	}
+	$result = "" | Select-Object -Property Name,IpAddress
+	$result.Name = $adapter.Name
+	$result.IpAddress = $ipSettings[0].IpAddress
+	$results += $result
 }
+
+return $results
