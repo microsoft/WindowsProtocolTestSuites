50700856bb649981f0be0155076330dc04f074cf
Michael Tsai <hstsai@microsoft.com>
2018-07-02 16:49:42 +0800

    Refine setVs2017Path batch and upgrade .net framework for SMBDPlugin

--- common/setVs2017Path.cmd
+++ common/setVs2017Path.cmd
@@ -5,18 +5,30 @@
 
 :: Find vs2017path in registry
 set REGEXE="%windir%\System32\reg.exe"
-set KEY_NAME="HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7"
+set KEY_NAME_32="HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\VisualStudio\SxS\VS7"
+set KEY_NAME_64="HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7"
 set VALUE_NAME=15.0
-set REG_QUERY_VS_2017_PATH=%REGEXE% QUERY %KEY_NAME% /v %VALUE_NAME%
+set REG_QUERY_VS2017_PATH_32BIT=%REGEXE% QUERY %KEY_NAME_32% /v %VALUE_NAME%
+set REG_QUERY_VS2017_PATH_64BIT=%REGEXE% QUERY %KEY_NAME_64% /v %VALUE_NAME%
 
 set vs2017path=
 
-%REG_QUERY_VS_2017_PATH%
+%REG_QUERY_VS2017_PATH_64BIT%
 if ErrorLevel 1 (
-    echo Visual Studio 2017 is not installed.
-    exit /b 0
+    :: Cannot find 64-bit path, try to find 32-bit path
+    %REG_QUERY_VS2017_PATH_32BIT%
+    if ErrorLevel 1 (
+        echo Visual Studio 2017 is not installed.
+        exit /b 0
+    ) else (
+        FOR /F "usebackq tokens=1-2*" %%A IN (`"%REG_QUERY_VS2017_PATH_32BIT%"`) DO (
+            set name=%%A
+            set type=%%B
+            set vs2017path=%%C
+        )
+    )
 ) else (
-    FOR /F "usebackq tokens=1-2*" %%A IN (`"%REG_QUERY_VS_2017_PATH%"`) DO (
+    FOR /F "usebackq tokens=1-2*" %%A IN (`"%REG_QUERY_VS2017_PATH_64BIT%"`) DO (
         set name=%%A
         set type=%%B
         set vs2017path=%%C
