6c87727dd6f56efd169254f1ab3fe45f19712f7b
Michael Tsai <hstsai@microsoft.com>
2018-07-14 20:28:55 -0700

    Update msbuild and vspath order

--- common/setVsPath.cmd
+++ common/setVsPath.cmd
@@ -3,36 +3,30 @@
 
 @echo off
 
+:: Find installed path of Visual Studio 2017
+set _currentPath=%~dp0
+call "%_currentPath%setVs2017Path.cmd"
+if ErrorLevel 1 (
+    exit /b 1
+)
+
 :: Set vspath
 if not defined vspath (
-    if defined VS110COMNTOOLS (
-        set vspath="%VS110COMNTOOLS%"
-        goto end
-    ) else if defined VS120COMNTOOLS (
-        set vspath="%VS120COMNTOOLS%"
+    if defined VS150COMNTOOLS (
+        set vspath="%VS150COMNTOOLS%"
         goto end
     ) else if defined VS140COMNTOOLS (
         set vspath="%VS140COMNTOOLS%"
         goto end
+    ) else if defined VS120COMNTOOLS (
+        set vspath="%VS120COMNTOOLS%"
+        goto end
+    ) else if defined VS110COMNTOOLS (
+        set vspath="%VS110COMNTOOLS%"
+        goto end
     ) else (
-        :: Find installed path of Visual Studio 2017
-        set _currentPath=%~dp0
-        call "%_currentPath%setVs2017Path.cmd"
-        if ErrorLevel 1 (
-            exit /b 1
-        )
-
-        :: Set VS150COMNTOOLS
-        if exist "%vs2017path%\Common7\Tools\" (
-            set VS150COMNTOOLS="%vs2017path%\Common7\Tools\"
-        )
-        if defined VS150COMNTOOLS (
-            set vspath="%VS150COMNTOOLS%"
-            goto end
-        ) else (
-            echo Error: Visual Studio or Visual Studio test agent should be installed (version 2012, 2013, 2015 or 2017)
-            exit /b 1
-        )
+        echo Error: Visual Studio or Visual Studio test agent should be installed (version 2012, 2013, 2015 or 2017)
+        exit /b 1
     )
 )
 
