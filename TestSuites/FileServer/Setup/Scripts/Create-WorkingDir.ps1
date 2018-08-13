fe5f4043bc1b23476ee5d2a26a497ab931aa7e6f
FAREAST\v-suxu <v-suxu@microsoft.com>
2018-08-13 16:30:36 +0800

    prepare working dir for customer not effect regression run

---
+++ TestSuites/FileServer/Setup/Scripts/Create-WorkingDir.ps1
@@ -0,0 +1,20 @@
+﻿#############################################################################
+## Copyright (c) Microsoft. All rights reserved.
+## Licensed under the MIT license. See LICENSE file in the project root for full license information.
+##
+#############################################################################
+
+param($workingDir = "$env:SystemDrive\Temp", $protocolConfigFile = "$workingDir\Protocol.xml")
+
+$scriptPath = Split-Path $MyInvocation.MyCommand.Definition -parent
+
+if(!(Test-Path "$workingDir"))
+{
+    New-Item -ItemType directory -Path $workingDir
+}
+
+if(!(Test-Path "$protocolConfigFile"))
+{
+    $protocolConfigFile = "$scriptPath\Protocol.xml"
+    Copy-Item $protocolConfigFile -destination $workingDir
+}
