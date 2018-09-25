5bff18190446016415943ed945e314be05d7c4db
Michael Tsai <hstsai@microsoft.com>
2018-09-25 14:37:03 +0800

    Fix installprerequisites
    1. Add PTF in xml file for reuse purpose
    2. Remove --quiet in InstallVs2017Community.cmd
    3. Use msiexec.exe to install downloaded msi/exe first; if failed, use
    original method to perform installation

--- InstallPrerequisites/InstallPrerequisites.ps1
+++ InstallPrerequisites/InstallPrerequisites.ps1
@@ -271,11 +271,19 @@ Function DownloadAndInstallApplication
         $ExitCode = 0
         if ($AppItem.Arguments.Trim().Length -lt 1 )
         {
-            $ExitCode = (Start-Process -FILEPATH $OutputPath -Wait -PassThru).ExitCode
+            $ExitCode = (Start-Process -FILEPATH "$env:systemroot\system32\msiexec.exe" -ArgumentList "/i $OutputPath /passive" -Wait -PassThru).ExitCode
+            if ($ExitCode -NE 0)
+            {
+                $ExitCode = (Start-Process -FILEPATH $OutputPath -Wait -PassThru).ExitCode
+            }
         }
         else
         {        
-            $ExitCode = (Start-Process -FILEPATH $OutputPath $FLAGS -Wait -PassThru).ExitCode
+            $ExitCode = (Start-Process -FILEPATH "$env:systemroot\system32\msiexec.exe" -ArgumentList "/i $OutputPath $FLAGS /passive" -Wait -PassThru).ExitCode
+            if ($ExitCode -NE 0)
+            {
+                $ExitCode = (Start-Process -FILEPATH $OutputPath $FLAGS -Wait -PassThru).ExitCode
+            }
         }
         
         if ($ExitCode -EQ 0)
@@ -297,7 +305,9 @@ Function DownloadAndInstallApplication
         }
     }
 }
-$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
 
 # Start get all needed tools from configure file.
 $downloadList = GetDownloadTools -DpConfigPath $ConfigPath -ToolCategory $Category
