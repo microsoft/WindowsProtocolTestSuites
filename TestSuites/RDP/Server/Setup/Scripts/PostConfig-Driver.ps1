d9d64cd359e57e4dcb6a23dafd22867ab0a7c1b0
FAREAST\v-xinzli <v-xinzli@microsoft.com>
2018-11-19 17:59:56 +0800

    Move scripts of RDPServer from Herlper to Test

---
+++ TestSuites/RDP/Server/Setup/Scripts/PostConfig-Driver.ps1
@@ -0,0 +1,127 @@
+#############################################################
+## Copyright (c) Microsoft Corporation. All rights reserved.
+## Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#############################################################
+
+#############################################################################
+##
+## Microsoft Windows Powershell Sripting
+## File         :   PostConfig-Driver.ps1
+## Requirements :   Windows Powershell 3.0
+## Supported OS :   Windows Server 2012R2 or later
+##
+##############################################################################
+
+Param
+(
+	[string]$WorkingPath = "C:\temp"
+)
+
+$param                   = @{}
+$ScriptFileFullPath      = $MyInvocation.MyCommand.Definition
+$ScriptName              = [System.IO.Path]::GetFileName($ScriptFileFullPath)
+$LogFileFullPath         = "$ScriptFileFullPath.log"
+$SignalFileFullPath      = "$WorkingPath\post.finished.signal"
+[string]$configPath	 	 = "$WorkingPath\protocol.xml"
+
+[xml]$Content = Get-Content $configPath
+$driverComputerSettting = $Content.lab.servers.vm | where {$_.role -eq "DriverComputer"}
+$coreSettting = $Content.lab.core
+
+#------------------------------------------------------------------------------------------
+# Function: Start-ConfigLog
+# Create log file and start logging
+#------------------------------------------------------------------------------------------
+Function Start-ConfigLog()
+{
+    if(!(Test-Path -Path $LogFileFullPath)){
+        New-Item -ItemType File -path $LogFileFullPath -Force
+    }
+    Start-Transcript $LogFileFullPath -Append 2>&1 | Out-Null
+}
+
+#------------------------------------------------------------------------------------------
+# Function: Write-ConfigLog
+# Write information to log file
+#------------------------------------------------------------------------------------------
+Function Write-ConfigLog
+{
+    Param (
+        [Parameter(ValueFromPipeline=$true)] $text,
+        $ForegroundColor = "Green"
+    )
+
+    $date = Get-Date -f MM-dd-yyyy_HH_mm_ss
+    Write-Output "[$date] $text"
+}
+
+Function Complete-Configure
+{
+    # Write signal file
+    Write-ConfigLog "Write signal file`: post.finished.signal to hard drive."
+    cmd /C ECHO CONFIG FINISHED > $SignalFileFullPath
+
+    # Ending script
+    Write-ConfigLog "Config finished."
+    Write-ConfigLog "EXECUTE [$ScriptName] FINISHED (NOT VERIFIED)." -ForegroundColor Green
+    Stop-Transcript
+
+    Restart-Computer;
+}
+
+#------------------------------------------------------------------------------------------
+# Function: Init-Environment
+# Start logging, check signal file, switch to script path and read the config parameters
+#------------------------------------------------------------------------------------------
+Function Init-Environment()
+{
+    # Start logging
+    Start-ConfigLog
+
+    # Start executing the script
+    Write-ConfigLog "Executing [$ScriptName]..." -ForegroundColor Cyan
+
+    # Switch to the working path
+    Write-ConfigLog "Switching to $WorkingPath..." -ForegroundColor Yellow
+    Push-Location $WorkingPath
+}
+
+#------------------------------------------------------------------------------------------
+# Function: Config-Environment
+# Control the overall workflow of all configuration phases
+#------------------------------------------------------------------------------------------
+Function Config-Environment
+{
+    # Start configure
+	[string] $domain = $driverComputerSettting.domain
+	[string] $userName = $coreSettting.username
+	[string] $userPwd = $coreSettting.password
+	
+    Write-ConfigLog "Setting autologon..." -ForegroundColor Yellow
+    Set-AutoLogon -Domain $domain -Username $userName -Password $userPwd -Count 999
+
+    # Turn off UAC
+    Write-ConfigLog "Turn off UAC..." -ForegroundColor Yellow
+    Set-ItemProperty -path  HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -name "EnableLUA" -value "0"
+
+    Write-Host "Enable remoting"
+    Enable-Remoting
+}
+
+
+#------------------------------------------------------------------------------------------
+# Main Function
+#------------------------------------------------------------------------------------------
+Function Main
+{
+    # Initialize configure environment
+    Init-Environment    
+
+    # Start configure
+    Config-Environment
+
+    # Complete configure
+    Complete-Configure
+}
+
+Main
---
+++ TestSuites/RDP/Server/Setup/Scripts/PostConfig-SUT.ps1
@@ -0,0 +1,135 @@
+##################################################################################
+## Copyright (c) Microsoft Corporation. All rights reserved.
+## Licensed under the MIT license. See LICENSE file in the project root for full license information.
+##################################################################################
+
+#############################################################################
+##
+## Microsoft Windows Powershell Sripting
+## File         :   PostConfig-SUT.ps1
+## Requirements :   Windows Powershell 3.0
+## Supported OS :   Windows Server 2012R2
+##
+##############################################################################
+
+Param
+(
+    [string]$WorkingPath = "C:\temp"
+)
+
+$param                   = @{}
+$ScriptFileFullPath      = $MyInvocation.MyCommand.Definition
+$ScriptName              = [System.IO.Path]::GetFileName($ScriptFileFullPath)
+$LogFileFullPath         = "$ScriptFileFullPath.log"
+$SignalFileFullPath      = "$WorkingPath\post.finished.signal"
+$configPath				 = "$WorkingPath\protocol.xml"
+
+[xml]$Content = Get-Content $configPath
+$sutSettting = $Content.lab.servers.vm | where {$_.role -eq "SUT"}
+$coreSettting = $Content.lab.core
+
+#------------------------------------------------------------------------------------------
+# Function: Start-ConfigLog
+# Create log file and start logging
+#------------------------------------------------------------------------------------------
+Function Start-ConfigLog()
+{
+    if(!(Test-Path -Path $LogFileFullPath)){
+        New-Item -ItemType File -path $LogFileFullPath -Force
+    }
+    Start-Transcript $LogFileFullPath -Append 2>&1 | Out-Null
+}
+
+#------------------------------------------------------------------------------------------
+# Function: Write-ConfigLog
+# Write information to log file
+#------------------------------------------------------------------------------------------
+Function Write-ConfigLog
+{
+    Param (
+        [Parameter(ValueFromPipeline=$true)] $text,
+        $ForegroundColor = "Green"
+    )
+
+    $date = Get-Date -f MM-dd-yyyy_HH_mm_ss
+    Write-Output "[$date] $text"
+}
+
+Function Complete-Configure
+{
+    # Write signal file
+    Write-ConfigLog "Write signal file`: post.finished.signal to hard drive."
+    cmd /C ECHO CONFIG FINISHED > $SignalFileFullPath
+
+    # Ending script
+    Write-ConfigLog "Config finished."
+    Write-ConfigLog "EXECUTE [$ScriptName] FINISHED (NOT VERIFIED)." -ForegroundColor Green
+    Stop-Transcript
+
+    Restart-Computer;
+}
+
+#------------------------------------------------------------------------------------------
+# Function: Init-Environment
+# Start logging, check signal file, switch to script path and read the config parameters
+#------------------------------------------------------------------------------------------
+Function Init-Environment()
+{
+    # Start logging
+    Start-ConfigLog
+
+    # Start executing the script
+    Write-ConfigLog "Executing [$ScriptName]..." -ForegroundColor Cyan
+
+    # Switch to the working path
+    Write-ConfigLog "Switching to $WorkingPath..." -ForegroundColor Yellow
+    Push-Location $WorkingPath
+}
+
+#------------------------------------------------------------------------------------------
+# Function: Config-Environment
+# Control the overall workflow of all configuration phases
+#------------------------------------------------------------------------------------------
+Function Config-Environment
+{
+	[string] $domain = $sutSettting.domain
+	[string] $userName = $coreSettting.username
+	[string] $userPwd = $coreSettting.password
+	
+    # Start configure
+    Write-ConfigLog "Setting autologon..." -ForegroundColor Yellow
+    Set-AutoLogon -Domain $domain -Username $userName -Password $userPwd -Count 999
+
+    # Turn off UAC
+    Write-ConfigLog "Turn off UAC..." -ForegroundColor Yellow
+    Set-ItemProperty -path  HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -name "EnableLUA" -value "0"
+
+    Write-Host "Enable remoting"
+    Enable-Remoting
+	
+	# Enable Remote Desktop
+	(Get-WmiObject Win32_TerminalServiceSetting -Namespace root\cimv2\TerminalServices).SetAllowTsConnections(1,1) | Out-Null
+	(Get-WmiObject -Class "Win32_TSGeneralSetting" -Namespace root\cimv2\TerminalServices -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0) | Out-Null
+	Get-NetFirewallRule -DisplayName "Remote Desktop*" | Set-NetFirewallRule -enabled true
+	
+	# Configure Network detection on RDP Server
+	Set-ItemProperty -path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -name "SelectNetworkDetect" -value "0"
+}
+
+
+#------------------------------------------------------------------------------------------
+# Main Function
+#------------------------------------------------------------------------------------------
+Function Main
+{
+    # Initialize configure environment
+    Init-Environment    
+
+    # Start configure
+    Config-Environment
+
+    # Complete configure
+    Complete-Configure
+}
+
+Main
