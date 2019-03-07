a6de41c0c2543fd576c410489aacf2bfeeb2a778
Yan Zeng <yazeng@microsoft.com>
2019-03-07 02:24:08 +0000

    Merged PR 939: Fix RDP client PTM bugs
    Cherry picked from !937

--- TestSuites/RDP/Client/src/Adapter/NonWindowsSUTImplementation/TriggerClientDisconnectAll.ps1
+++
@@ -1,4 +0,0 @@
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-#This method is used to trigger RDP client to close all RDP connection to server for clean up.
