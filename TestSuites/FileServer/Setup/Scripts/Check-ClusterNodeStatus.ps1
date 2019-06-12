9627920504c1d4013f70ed9892a59907205adff4
jchpyll <1668327570@qq.com>
2019-06-12 16:57:32 +0800

    Update Check-ClusterNodeStatus.ps1

--- TestSuites/FileServer/Setup/Scripts/Check-ClusterNodeStatus.ps1
+++ TestSuites/FileServer/Setup/Scripts/Check-ClusterNodeStatus.ps1
@@ -3,7 +3,7 @@
 ## Licensed under the MIT license. See LICENSE file in the project root for full license information.
 ##
 #############################################################################
-
+#test
 param(
     $workingDir = "$env:SystemDrive\Temp",
     [ValidateSet("CreateCheckerTask", "StartChecker")]
