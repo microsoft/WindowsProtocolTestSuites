cf049041fb155cb4eeaf32cc8f665b907209eb4f
Kyle Zhang <v-zhanh@microsoft.com>
2019-06-12 16:57:36 +0800

    update

--- TestSuites/RDP/Client/Setup/Scripts/Config-TerminalClient.ps1
+++ TestSuites/RDP/Client/Setup/Scripts/Config-TerminalClient.ps1
@@ -258,7 +258,7 @@ if($driverComputerIP -ne $driverComputerName)
 #-----------------------------------------------------
 Pop-Location
 Write-Host "Write signal file: config.finished.signal to system drive."
-cmd /C ECHO CONFIG FINISHED>$env:HOMEDRIVE\config.finished.signal
+cmd /C ECHO CONFIG FINISHED>$env:HOMEDRIVE\configTC.finished.signal
 
 #----------------------------------------------------------------------------
 # Ending script
