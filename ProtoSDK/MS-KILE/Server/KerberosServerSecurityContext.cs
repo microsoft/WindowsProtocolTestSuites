7b8b5312654cd2b6ea3e1538df4d4e2854c1f996
Youssef1313 <31348972+Youssef1313@users.noreply.github.com>
2019-07-09 17:27:28 +0200

    Use var when data type is clear.

--- ProtoSDK/MS-KILE/Server/KerberosServerSecurityContext.cs
+++ ProtoSDK/MS-KILE/Server/KerberosServerSecurityContext.cs
@@ -118,7 +118,7 @@ namespace Microsoft.Protocols.TestTools.StackSdk.Security.Kile
             }
             if (isInitialToken)
             {
-                KileApRequest apRequest = new KileApRequest(server.Context);
+                var apRequest = new KileApRequest(server.Context);
                 apRequest.FromBytes(inToken, ticketEncryptKey);
                 bool isMutualAuth = (apRequest.Request.ap_options.ByteArrayValue[0] << 24 & (int)ApOptions.MutualRequired)
                     == (int)ApOptions.MutualRequired;
@@ -126,12 +126,12 @@ namespace Microsoft.Protocols.TestTools.StackSdk.Security.Kile
 
                 if (isMutualAuth || isDceStyle)
                 {
-                    EncryptionKey apSubKey = new EncryptionKey(new KerbInt32((int)EncryptionType.RC4_HMAC),
+                    var apSubKey = new EncryptionKey(new KerbInt32((int)EncryptionType.RC4_HMAC),
                         new Asn1OctetString(Guid.NewGuid().ToByteArray()));
                     KileApResponse apResponse = server.CreateApResponse(apSubKey);
 
                     // Set a random sequence number
-                    Random randomNumber = new Random();
+                    var randomNumber = new Random();
                     apResponse.ApEncPart.seq_number = new KerbUInt32(randomNumber.Next());
                     server.context.currentLocalSequenceNumber = (ulong)apResponse.ApEncPart.seq_number.Value;
                     token = apResponse.ToBytes();
@@ -151,7 +151,7 @@ namespace Microsoft.Protocols.TestTools.StackSdk.Security.Kile
             }
             else
             {
-                KileApResponse apResponse = new KileApResponse(server.Context);
+                var apResponse = new KileApResponse(server.Context);
                 apResponse.FromBytes(inToken);
 
                 if (server.Context.CurrentLocalSequenceNumber != (ulong)apResponse.ApEncPart.seq_number.Value)
