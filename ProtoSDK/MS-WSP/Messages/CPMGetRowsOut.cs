893543de6059908240db40adcaca3385e73361be
Yan Zeng <yazeng@microsoft.com>
2019-08-19 14:29:37 +0800

    initial commit

---
+++ ProtoSDK/MS-WSP/Messages/CPMGetRowsOut.cs
@@ -0,0 +1,109 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Microsoft.Protocols.TestTools.StackSdk.FileAccessService.WSP
+{
+    /// <summary>
+    /// The CPMCreateRowsOut message contains a response to a CPMCreateRowsIn message.
+    /// </summary>
+    public struct CPMCreateRowsOut : IWspOutMessage
+    {
+        /// <summary>
+        /// A 32-bit unsigned integer indicating the number of rows returned in Rows.
+        /// </summary>
+        public UInt32 _cRowsReturned;
+
+        /// <summary>
+        /// A 32-bit unsigned integer. 
+        /// </summary>
+        public RowSeekType eType;
+
+        /// <summary>
+        /// A 32-bit value representing the handle of the rowset chapter.
+        /// </summary>
+        public UInt32 _chapt;
+
+        /// <summary>
+        /// This field MUST contain a structure of the type indicated by the eType field.
+        /// </summary>
+        public object SeekDescription;
+
+        /// <summary>
+        /// Row data is formatted as prescribed by column information in the most recent CPMSetBindingsIn message. 
+        /// The fixed-sized area (at the beginning of the row buffer) MUST contain a CTableVariant for each column, 
+        /// stored at the offset specified in the most recent CPMSetBindingsIn message. 
+        /// </summary>
+        public CTableVariant[] tableVariants;
+
+        /// <summary>
+        /// The actual data of the rows, stored starting at the end of the buffer.
+        /// </summary>
+        public string[] RowsData;
+
+        public IWspInMessage Request { get; set; }
+
+        public WspMessageHeader Header { get; set; }
+
+
+        public void FromBytes(WspBuffer buffer)
+        {
+            var header = new WspMessageHeader();
+
+            header.FromBytes(buffer);
+
+            Header = header;
+
+            _cRowsReturned = buffer.ToStruct<UInt32>();
+
+            eType = buffer.ToStruct<RowSeekType>();
+
+            _chapt = buffer.ToStruct<UInt32>();
+
+            switch (eType)
+            {
+                case RowSeekType.eRowSeekNone:
+                    SeekDescription = null;
+                    break;
+                case RowSeekType.eRowSeekNext:
+                    SeekDescription = buffer.ToStruct<CRowSeekNext>();
+                    break;
+                case RowSeekType.eRowSeekAt:
+                    SeekDescription = buffer.ToStruct<CRowSeekAt>();
+                    break;
+                case RowSeekType.eRowSeekAtRatio:
+                    SeekDescription = buffer.ToStruct<CRowSeekAtRatio>();
+                    break;
+                case RowSeekType.eRowSeekByBookmark:
+                    SeekDescription = buffer.ToStruct<CRowSeekByBookmark>();
+                    break;
+            }
+
+
+
+            int categorizationCount = 0;
+
+            var createQueryIn = (CPMCreateQueryIn)Request;
+
+            if (createQueryIn.CCategorizationSet.HasValue)
+            {
+                categorizationCount = (int)createQueryIn.CCategorizationSet.Value.count;
+            }
+
+            aCursors = new UInt32[categorizationCount + 1];
+
+            for (int i = 0; i < aCursors.Length; i++)
+            {
+                UInt32 cursor = buffer.ToStruct<UInt32>();
+
+                aCursors[i] = cursor;
+            }
+        }
+
+        public void ToBytes(WspBuffer buffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProtoSDK/MS-WSP/Structures/CTableVariant.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Microsoft.Protocols.TestTools.StackSdk.FileAccessService.WSP
+{
+    /// <summary>
+    /// The CTableVariant structure contains the fixed-size portion of a variable length data type stored in the CPMGetRowsOut message.
+    /// </summary>
+    public struct CTableVariant : IWspStructure
+    {
+        /// <summary>
+        /// A type indicator, indicating the type of vValue. It MUST be one of the values under the vType field, as specified in section 2.2.1.1.
+        /// </summary>
+        public ushort vType;
+
+        /// <summary>
+        /// Not used. Can be set to any arbitrary value when sent and it MUST be ignored on receipt.
+        /// </summary>
+        public ushort reserved1;
+
+        /// <summary>
+        /// Not used. Can be set to any arbitrary value when sent and it MUST be ignored on receipt.
+        /// </summary>
+        public ushort reserved2;
+        /// <summary>
+        /// Element count. This field is 4 bytes and is present only if the vType is a VT_VECTOR.
+        /// </summary>
+        public UInt32? Count;
+
+        /// <summary>
+        /// An offset to variable length data (for example, a string). 
+        /// This MUST be a 32-bit value (4 bytes long) if 32-bit offsets are being used (per the rules in section 2.2.3.12), or a 64-byte value (8 bytes long) if 64-bit offsets are being used.
+        /// </summary>
+        public object Offset;
+
+        public void FromBytes(WspBuffer buffer)
+        {
+
+        }
+
+        public void ToBytes(WspBuffer buffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
