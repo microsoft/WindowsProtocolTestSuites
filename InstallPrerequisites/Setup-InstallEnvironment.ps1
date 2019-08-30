425b0d7d3fb245dc281b4cb658d4daf7eaa2604e
Grom <v-xinzli@microsoft.com>
2019-08-30 17:50:38 +0800

    Update Setup-InstallEnvironment.ps1

--- InstallPrerequisites/Setup-InstallEnvironment.ps1
+++ InstallPrerequisites/Setup-InstallEnvironment.ps1
@@ -12,13 +12,18 @@ Param
 
 $InvocationPath = Split-Path $MyInvocation.MyCommand.Definition -parent
 $WinteropProtocolTesting = "$Workspace\..\..\WinteropProtocolTesting"
+$HostOsBuildNumber       = "" + [Environment]::OSVersion.Version.Major + "." + [Environment]::OSVersion.Version.Minor
 Write-Host "=============================================="
 Write-Host "InvocationPath: $InvocationPath"
 Write-Host "Workspace： $Workspace"
 Write-Host "WinteropProtocolTesting: $WinteropProtocolTesting"
 Write-Host "=============================================="
 
-function Get-VHD {
+#------------------------------------------------------------------------------------------
+# Download VHD from share folder to workspace
+#------------------------------------------------------------------------------------------
+
+function Download-VHD {
     if(!(Test-Path "$VHDPath\$VHDName")){
         Write-Host "Cannot find the VHD on $VHDPath\$VHDName" Exit
     }
@@ -31,8 +36,58 @@ function Get-VHD {
     Write-Host "Copy VHD finished"
 }
 
-function Main {
-    Get-VHD
+#------------------------------------------------------------------------------------------
+# Check the prerequisites of the host machine before setup test suite environment
+#------------------------------------------------------------------------------------------
+Function Check-HostPrerequisites {
+
+    Write-TestSuiteInfo "Check prerequisites of the host for test suite environment setup:"
+
+    Write-TestSuiteStep "Check if the host operating system version is supported or not."
+    if ([Double]$Script:HostOsBuildNumber -le [Double]"6.1") {
+        Write-TestSuiteError "Unsupported operating system version $Script:HostOsBuildNumber. Must be larger than 6.1." -Exit
+    }
+    else {
+        Write-TestSuiteSuccess "Supported operating system version $Script:HostOsBuildNumber."
+    }
+
+    Write-TestSuiteStep "Check if the host has enabled router by registry key."
+    # http://technet.microsoft.com/en-us/library/cc962461.aspx
+    If ((Get-ItemProperty -path HKLM:\system\CurrentControlSet\services\Tcpip\Parameters -name IpEnableRouter -ErrorAction Silentlycontinue).ipenablerouter -ne 1) {
+        Write-TestSuiteWarning "Router is disabled. Registry key IpEnableRouter under path HKLM:\system\CurrentControlSet\services\Tcpip\Parameters is not set to 1. Set it now..."
+        Set-ItemProperty -Path HKLM:\system\CurrentControlSet\services\Tcpip\Parameters -Name IpEnableRouter -Value 1
+    }
+    else {
+        Write-TestSuiteSuccess "Router is enabled."
+    }
+
+    Write-TestSuiteStep "Check if `"RSAT-Hyper-V-Tools`" feature is installed or not."
+    Write-TestSuiteInfo "Import ServerManager module if not imported."
+    Import-Module ServerManager
+    $FeatureName = "RSAT-Hyper-V-Tools"
+    $Feature = Get-WindowsFeature | Where { $_.Name -eq "$FeatureName" }
+    if($Feature.Installed -eq $false) {
+        Write-TestSuiteWarning "Feature not installed. Install it now..."
+        Add-WindowsFeature -Name $FeatureName -IncludeAllSubFeature -IncludeManagementTools
+        Wait-TestSuiteActivityComplete -ActivityName "Install $FeatureName" -TimeoutInSeconds 5
+    }
+    else {
+        Write-TestSuiteSuccess "Feature already installed."
+    }
+    
+    Write-TestSuiteStep "Check if `"Hyper-V v3.0 PowerShell Module`" is imported:"
+    if (!(Get-Module -ListAvailable Hyper-V)) {
+        Write-TestSuiteWarning "Module not imported. Import it now..."
+        Import-Module Hyper-V
+    } 
+    else {
+        Write-TestSuiteSuccess "Module already imported."
+    }
+}
+
+function Main {    
+    Download-VHD
+    Check-HostPrerequisites
 }
 
 Main
