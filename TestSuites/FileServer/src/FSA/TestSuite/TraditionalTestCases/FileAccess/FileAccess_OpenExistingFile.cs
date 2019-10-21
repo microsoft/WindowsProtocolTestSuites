17625588ec1661d46cc6c6858ed3632a4806b377
Lu Chen (IAT) <lche@microsoft.com>
2019-10-21 10:31:24 +0800

    update

--- TestSuites/FileServer/src/FSA/TestSuite/TraditionalTestCases/FileAccess/FileAccess_OpenExistingFile.cs
+++ TestSuites/FileServer/src/FSA/TestSuite/TraditionalTestCases/FileAccess/FileAccess_OpenExistingFile.cs
@@ -18,38 +18,52 @@ namespace Microsoft.Protocols.TestSuites.FileSharing.FSA.TestSuite
         [TestCategory(TestCategories.FileAccess)]
         [TestCategory(TestCategories.NonSmb)]
         [Description("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")]
-        public void CreateFile_InvalidStreamName()
+        public void FileAccess_WriteReadOnlyFile()
+        {
+            string fileName = this.fsaAdapter.ComposeRandomFileName(8);
+            FileAccess_Create_ReadOnlyFile(fileName);
+
+            BaseTestSite.Log.Add(LogEntryKind.TestStep, "3. Open the existing file with write access");
+
+            MessageStatus status = this.fsaAdapter.CreateFile(
+                        fileName,
+                        FileAttribute.NORMAL,
+                        CreateOptions.NON_DIRECTORY_FILE,
+                        FileAccess.FILE_WRITE_DATA,
+                        ShareAccess.FILE_SHARE_READ,
+                        CreateDisposition.CREATE);
+
+            //Step 2: Verify test result
+            BaseTestSite.Log.Add(LogEntryKind.TestStep, "4. Verify returned NTSTATUS code.");
+            this.fsaAdapter.AssertAreEqual(this.Manager, MessageStatus.ACCESS_DENIED, status,
+                    "xxxxxxxxxxxxxxxxxxx");
+
+        }
+
+        #endregion
+
+        #region Test Case Utility
+
+        private void FileAccess_Create_ReadOnlyFile(string fileName)
         {
             BaseTestSite.Log.Add(LogEntryKind.TestStep, "Test case steps:");
             MessageStatus status;
 
-            //Step 1: Create File
-            BaseTestSite.Log.Add(LogEntryKind.TestStep, "1. Create a directory with invalid stream name");
-
-            string fileName = this.fsaAdapter.ComposeRandomFileName(8);
-            string streamName = this.fsaAdapter.ComposeRandomFileName(8);
-            string randomFile = fileName + ":" + streamName + ":$INDEX_ALLOCATION";
+            //Step 1: Create read only file
+            BaseTestSite.Log.Add(LogEntryKind.TestStep, "1. Create a read only data file");            
 
             status = this.fsaAdapter.CreateFile(
-                        randomFile,
-                        FileAttribute.NORMAL,
-                        CreateOptions.DIRECTORY_FILE,
-                        FileAccess.GENERIC_ALL,
+                        fileName,
+                        FileAttribute.READONLY,
+                        CreateOptions.NON_DIRECTORY_FILE,
+                        FileAccess.FILE_WRITE_DATA,
                         ShareAccess.FILE_SHARE_READ,
                         CreateDisposition.CREATE);
 
             //Step 2: Verify test result
             BaseTestSite.Log.Add(LogEntryKind.TestStep, "2. Verify returned NTSTATUS code.");
-            if (this.fsaAdapter.FileSystem == FileSystem.NTFS || this.fsaAdapter.FileSystem == FileSystem.REFS)
-            {
-                this.fsaAdapter.AssertAreEqual(this.Manager, MessageStatus.INVALID_PARAMETER, status,
-                    "If StreamTypeNameToOpen is \"$INDEX_ALLOCATION\" and StreamNameToOpen has a value other than an empty string or \"$I30\", the operation SHOULD be failed with STATUS_INVALID_PARAMETER.");
-            }
-            else
-            {
-                this.fsaAdapter.AssertAreEqual(this.Manager, MessageStatus.OBJECT_NAME_INVALID, status,
-                    "Only the NTFS and ReFS file systems support complex name suffixes and StreamTypeNames. File systems that do not support this return STATUS_OBJECT_NAME_INVALID.");
-            }
+            this.fsaAdapter.AssertAreEqual(this.Manager, MessageStatus.SUCCESS, status,
+                    "Create a read only file should succeed.");
         }
 
         #endregion
