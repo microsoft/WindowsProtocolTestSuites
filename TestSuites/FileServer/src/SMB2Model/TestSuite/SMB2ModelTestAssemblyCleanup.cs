c5c8a162e01a021d9f65639d205fb950e7e39bca
Minghao Hu <v-minghu@microsoft.com>
2019-11-21 11:02:57 +0800

    Add AssemblyCleanup.

---
+++ TestSuites/FileServer/src/SMB2Model/TestSuite/SMB2ModelTestAssemblyCleanup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Protocols.TestSuites.FileSharing.SMB2Model.Adapter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Protocols.TestSuites.FileSharing.SMB2Model.TestSuite
+{
+    [TestClass]
+    public class SMB2ModelTestAssemblyCleanup
+    {
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            foreach (var directory in ModelManagedAdapterBase.AllTestDirectories)
+            {
+                try
+                {
+                    Directory.Delete(directory);
+                }
+                catch
+                {
+
+                }
+            }
+
+            foreach (var file in ModelManagedAdapterBase.AllTestFiles)
+            {
+                try
+                {
+                    File.Delete(file);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
