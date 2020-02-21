b85d93309b7a24b11126bb40c4cf74f7e70b0149
Grom <v-xinzli@microsoft.com>
2020-02-21 12:00:25 +0800

    Modify comments to trigger regression

--- TestSuites/RDP/Client/Setup/Scripts/Config-DriverComputer.ps1
+++ TestSuites/RDP/Client/Setup/Scripts/Config-DriverComputer.ps1
@@ -1,5 +1,8 @@
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#############################################################################
+## Copyright (c) Microsoft Corporation. All rights reserved.
+## Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#############################################################################
+
 
 Param(
 [String]$scriptsPath  = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
