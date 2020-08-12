04418e781fa4db327b83de8743e46caaf8f2119e
Kyle Zhang <v-zhanh@microsoft.com>
2020-08-12 09:10:57 +0000

    Merged PR 5037772: Clean up csproj files in dotnet-core branch
    - remove all testsetting files
    - Copy sut control adapter scripts when build
    - Reference Microsoft.DotNet.Analyzers.Compatibility in test suites csproj
    - clean up ptfconfig
    
    Related work items: #28223903

--- TestSuites/BranchCache/src/Adapter/WindowsSUTControlAdapter/IsLocalCacheExisted.ps1
+++ TestSuites/BranchCache/src/Adapter/WindowsSUTControlAdapter/IsLocalCacheExisted.ps1
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-$credential = ./WindowsSUTControlAdapter/CreatePSCredential.ps1
+$credential = ./CreatePSCredential.ps1
 $status = Invoke-Command -ComputerName $computerName {netsh branchcache show localcache} -Credential $credential
 
 if ($status -match "Active Current Cache Size\s*\= 0 Bytes")
