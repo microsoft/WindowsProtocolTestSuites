a51738a41388a7d0300ce33931917d49a5df95e4
Huiren Jiang <hujia@microsoft.com>
2020-12-15 10:03:31 +0000

    Merged PR 5499029: Check platform compatibility of CredSSP for RDPServer
    Related work items: #31066305

--- TestSuites/RDP/Server/src/TestSuite/RdpTestClassBase.cs
+++ TestSuites/RDP/Server/src/TestSuite/RdpTestClassBase.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.Protocols.TestSuites.Rdpbcgr;
-using Microsoft.Protocols.TestSuites.Rdpedyc;
-using Microsoft.Protocols.TestSuites.Rdpemt;
 using Microsoft.Protocols.TestTools;
 using Microsoft.Protocols.TestTools.StackSdk.RemoteDesktop.Rdpbcgr;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -37,11 +34,28 @@ namespace Microsoft.Protocols.TestSuites.Rdp
         protected override void TestInitialize()
         {
             testConfig = new TestConfig(Site);
+
+            CheckPlatformCompatibility();
         }
 
         protected override void TestCleanup()
         {
             base.TestCleanup();
+        }
+
+        /// <summary>
+        /// Check platform compatibility.
+        /// </summary>
+        private void CheckPlatformCompatibility()
+        {
+            // Check CredSSP, which is currently only supported on Windows.
+            if (testConfig.transportProtocol == EncryptedProtocol.NegotiationCredSsp || testConfig.transportProtocol == EncryptedProtocol.DirectCredSsp)
+            {
+                if (!OperatingSystem.IsWindows())
+                {
+                    BaseTestSite.Assume.Inconclusive("The transport protocols based on CredSSP are only supported on Windows.");
+                }
+            }
         }
         #endregion
     }
