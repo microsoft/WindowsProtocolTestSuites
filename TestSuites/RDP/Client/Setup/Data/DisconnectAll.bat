8b3feffcf9794c575e50db5c40e26452bd30020e
Harley Zhang <v-hazh@microsoft.com>
2020-12-24 16:04:23 +0800

    Try to fix RDPClient failed case S1_ConnectionTest_Disconnection_PositiveTest_ClientInitiated

--- TestSuites/RDP/Client/Setup/Data/DisconnectAll.bat
+++ TestSuites/RDP/Client/Setup/Data/DisconnectAll.bat
@@ -6,4 +6,6 @@ cmd /c schtasks /end  /TN DirectCredSSP_RDPConnect
 cmd /c schtasks /end  /TN DirectTls_RDPConnect
 cmd /c schtasks /end  /TN TriggerNetworkFailure
 cmd /c schtasks /end  /TN TriggerInputEvents
+cmd /c taskkill /T /IM mstsc.exe
+timeout /t 1
 cmd /c taskkill /F /IM mstsc.exe
