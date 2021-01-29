f7c944f4279aa242ede967f76cbbf047bfc6f68a
Minghao Hu <v-minghu@microsoft.com>
2021-01-29 13:52:20 +0800

    Fix typo in comments.

--- TestSuites/FileServer/src/Common/Adapter/SutCommonControlManagedAdapter.cs
+++ TestSuites/FileServer/src/Common/Adapter/SutCommonControlManagedAdapter.cs
@@ -31,7 +31,7 @@ namespace Microsoft.Protocols.TestSuites.FileSharing.Common.Adapter
         /// <summary>
         /// Get group members from domain or local computer.
         /// </summary>
-        /// <param name="target">Name of the domain or local compuer. Use FQDN for domain.</param>
+        /// <param name="target">Name of the domain or local computer. Use FQDN for domain.</param>
         /// <param name="adminUserName">Name of the user who has administrative privileges.</param>
         /// <param name="groupName">Name of the queried group.</param>
         /// <returns>All group members returned.</returns>
@@ -47,7 +47,7 @@ namespace Microsoft.Protocols.TestSuites.FileSharing.Common.Adapter
         /// <summary>
         /// Get groups from domain or local computer.
         /// </summary>
-        /// <param name="target">Name of the domain or local compuer. Use FQDN for domain.</param>
+        /// <param name="target">Name of the domain or local computer. Use FQDN for domain.</param>
         /// <param name="adminUserName">Name of the user who has administrative privileges.</param>
         /// <returns>All groups returned.</returns>
         public List<Group> GetGroups(string target, string adminUserName)
@@ -62,7 +62,7 @@ namespace Microsoft.Protocols.TestSuites.FileSharing.Common.Adapter
         /// <summary>
         /// Get users from domain or local computer.
         /// </summary>
-        /// <param name="target">Name of the domain or local compuer. Use FQDN for domain.</param>
+        /// <param name="target">Name of the domain or local computer. Use FQDN for domain.</param>
         /// <param name="adminUserName">Name of the user who has administrative privileges.</param>
         /// <returns>All users returned.</returns>
         public List<User> GetUsers(string target, string adminUserName)
@@ -77,7 +77,7 @@ namespace Microsoft.Protocols.TestSuites.FileSharing.Common.Adapter
         /// <summary> 
         /// Get the SID of a user from domain or local computer.
         /// </summary>
-        /// <param name="target">Name of the domain or local compuer. Use FQDN for domain.</param>
+        /// <param name="target">Name of the domain or local computer. Use FQDN for domain.</param>
         /// <param name="adminUserName">Name of the user who has administrative privileges.</param>
         /// <param name="userName">Name of the queried user.</param>
         /// <returns>SID of the user returned.</returns>
@@ -105,7 +105,7 @@ namespace Microsoft.Protocols.TestSuites.FileSharing.Common.Adapter
         /// <summary>
         /// Get a _WindowsIdentity instance from domain or local computer by the user name.
         /// </summary>
-        /// <param name="target">Name of the domain or local compuer. Use FQDN for domain.</param>
+        /// <param name="target">Name of the domain or local computer. Use FQDN for domain.</param>
         /// <param name="adminUserName">Name of the user who has administrative privileges.</param>
         /// <param name="userName">Name of the queried user.</param>
         /// <returns>A _WindowsIdentity instance represents the user.</returns>
