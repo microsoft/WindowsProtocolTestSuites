210657b1f4eca537c4822ee5e209c1bda8ecc040
Abiodun Aremu <abiodunaremu@microsoft.com>
2021-02-11 16:09:28 +0100

    Algorithm_Noting_FIleModified

---
+++ TestSuites/FileServer/src/FSA/TestSuite/CommonAlgorithm/Algorithm_Noting_FileModified.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Protocols.TestSuites.FileSharing.FSA.Adapter;
+using Microsoft.Protocols.TestSuites.FileSharing.Common.Adapter;
+using Microsoft.Protocols.TestTools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Protocols.TestSuites.FileSharing.FSA.TestSuite
+{
+    public partial class CommonAlgorithmTestCases : PtfTestClassBase
+    {
+        #region Test Cases
+
+        [TestMethod()]
+        [TestCategory(TestCategories.Fsa)]
+        [TestCategory(TestCategories.SetFileInformation)]
+        [TestCategory(TestCategories.NonSmb)]
+        [TestCategory(TestCategories.Positive)]
+        [Description("Try to set FileFullEaInformation to a file and check if ExtendedAttributes is supported.")]
+        public void FileInfo_Set_FileFullEaInformation_File_IsEASupported()
+        {
+            FileInfo_Set_FileFullEaInformation_IsEASupported(FileType.DataFile);
+        }
+
+        #endregion
+
+
+        #region Test Case Utility
+
+        private void FileInfo_Set_FileFullEaInformation_IsEASupported(FileType fileType)
+        {
+            BaseTestSite.Log.Add(LogEntryKind.TestStep, "Test case steps:");
+            MessageStatus status;
+
+            //Step 1: Create file
+            BaseTestSite.Log.Add(LogEntryKind.TestStep, "1. Create " + fileType.ToString());
+            status = this.fsaAdapter.CreateFile(fileType);
+        }
+
+        #endregion
+    }
+}
