c4b5ce91c7ca5cfafcccfaa3a574bd356c5cda44
Abiodun Aremu <abiodunaremu@microsoft.com>
2021-02-19 11:17:43 +0100

    Add lisence header to test case

--- TestSuites/FileServer/src/FSA/TestSuite/CommonAlgorithm/Algorithm_Noting_FileModified.cs
+++ TestSuites/FileServer/src/FSA/TestSuite/CommonAlgorithm/Algorithm_Noting_FileModified.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Protocols.TestSuites.FileSharing.FSA.Adapter;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Protocols.TestSuites.FileSharing.FSA.Adapter;
 using Microsoft.Protocols.TestSuites.FileSharing.Common.Adapter;
 using Microsoft.Protocols.TestTools;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
