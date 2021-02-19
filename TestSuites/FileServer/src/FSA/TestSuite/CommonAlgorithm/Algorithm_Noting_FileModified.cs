a709d9f5edac1732720fe30de12af7ddccce1f10
Abiodun Aremu <abiodunaremu@microsoft.com>
2021-02-19 09:48:47 +0100

    Add test case for [MS-FSA] section 2.1.4.17

--- TestSuites/FileServer/src/FSA/TestSuite/CommonAlgorithm/Algorithm_Noting_FileModified.cs
+++ TestSuites/FileServer/src/FSA/TestSuite/CommonAlgorithm/Algorithm_Noting_FileModified.cs
@@ -18,14 +18,13 @@ namespace Microsoft.Protocols.TestSuites.FileSharing.FSA.TestSuite
         [TestCategory(TestCategories.SetFileInformation)]
         [TestCategory(TestCategories.NonSmb)]
         [TestCategory(TestCategories.Positive)]
-        [Description("Try to set ")]
-        public void Algorithm_Noting_FileModified_Positive()
+        [Description("Try to write to file and check if file system consistent with [MS-FSA] section 2.1.4.17")]
+        public void Algorithm_Noting_FileModified()
         {
             Algorithm_Noting_FileModified(FileType.DataFile);
         }
 
         #endregion
-
 
         #region Test Case Utility
 
@@ -35,21 +34,37 @@ namespace Microsoft.Protocols.TestSuites.FileSharing.FSA.TestSuite
 
             //Step 1: Create file
             BaseTestSite.Log.Add(LogEntryKind.TestStep, "1. Create " + fileType.ToString());
-            
-            CreateFile(fileType);
 
-            //Query FileBasicInformation 
+            string fileName;
+            CreateFile(fileType, out fileName);
+
+            //Step 2: Query file basic information
+            BaseTestSite.Log.Add(LogEntryKind.TestStep, "2. Query FILE_BASIC_INFORMATION for timestamps");
+
             long oldCreationTime;
             long oldChangeTime;
             long oldLastAccessTime;
             long oldLastWriteTime;
 
-            QueryFileBasicInformation(out _, out oldChangeTime, out oldLastAccessTime, out oldLastWriteTime, out _);
+            QueryFileBasicInformation(out oldChangeTime, out oldCreationTime, out oldLastAccessTime, out oldLastWriteTime, out _);
+
+            //Step 3: Write to file
+            BaseTestSite.Log.Add(LogEntryKind.TestStep, "3. Write to file");
 
             //Write to file
             WriteToFile();
 
-            //Query FileBasicInformation
+            //Step 4: Close and open file
+            BaseTestSite.Log.Add(LogEntryKind.TestStep, "4. Close and open file to give significant time a file system can defer processing Section 2.1.4.17");
+
+            //<42> Section 2.1.4.17: File systems may choose to defer processing for a file that has been modified 
+            // to a later time, favoring performance over accuracy.
+            this.fsaAdapter.CloseOpen();
+            OpenFile(fileName);
+
+            //Step 5: Query FileBasicInformation
+            BaseTestSite.Log.Add(LogEntryKind.TestStep, "5. Query FILE_BASIC_INFORMATION for file attributes");
+
             long creationTime;
             long changeTime;
             long lastAccessTime;
@@ -58,34 +73,82 @@ namespace Microsoft.Protocols.TestSuites.FileSharing.FSA.TestSuite
 
             QueryFileBasicInformation(out changeTime, out creationTime, out lastAccessTime, out lastWriteTime, out isArchive);
 
-            //Verify timestamp updated appropriately
-            string oldChangeTimeDate = DateTime.FromFileTime(oldChangeTime).ToString();
-            string changeTimeDate = DateTime.FromFileTime(changeTime).ToString();
+            //Step 6: Verify that FileBasicInformation attributes are updated appropriately
+            BaseTestSite.Log.Add(LogEntryKind.TestStep, "6. Verify that FILE_BASIC_INFORMATION attributes are updated appropriately");
 
-            BaseTestSite.Assert.AreNotEqual(oldChangeTimeDate, changeTimeDate,
-                "The object store SHOULD set Open.File.LastChangeTime to the current system time.");
+            string oldCreationTimeDate = DateTime.FromFileTime(oldCreationTime).ToString();
+            string creationTimeDate = DateTime.FromFileTime(creationTime).ToString();
 
-            string oldLastWriteTimeDate = DateTime.FromFileTime(oldLastWriteTime).ToString();
-            string lastWriteTimeDate = DateTime.FromFileTime(lastWriteTime).ToString();
+            BaseTestSite.Assert.AreEqual(oldCreationTimeDate, creationTimeDate,
+                "File creation time is never updated in response to file system calls such as read and write.");
 
-            BaseTestSite.Assert.AreNotEqual(oldLastWriteTimeDate, lastWriteTimeDate,
-                "The object store SHOULD set Open.File.LastModificationTime to the current system time.");
+            DateTime oldLastWriteTimeDate = DateTime.FromFileTime(oldLastWriteTime);
+            DateTime lastWriteTimeDate = DateTime.FromFileTime(lastWriteTime);
 
-            string oldLastAccessTimeDate = DateTime.FromFileTime(oldLastAccessTime).ToString();
-            string lastAccessTimeDate = DateTime.FromFileTime(lastAccessTime).ToString();
+            DateTime oldChangeTimeDate = DateTime.FromFileTime(oldChangeTime);
+            DateTime changeTimeDate = DateTime.FromFileTime(changeTime);
 
-            BaseTestSite.Assert.AreNotEqual(oldLastAccessTimeDate, lastAccessTimeDate,
-                "The object store SHOULD set Open.File.LastAccessTime to the current system time.");
+            DateTime oldLastAccessTimeDate = DateTime.FromFileTime(oldLastAccessTime);
+            DateTime lastAccessTimeDate = DateTime.FromFileTime(lastAccessTime);
 
-            //Verify FILE_ATTRIBUTE_ARCHIVE is set to TRUE
-            BaseTestSite.Assert.IsTrue(isArchive,
-                "The object store SHOULD set Open.File.LastAccessTime to the current system time.");
-            
+            if (oldLastWriteTimeDate.CompareTo(lastWriteTimeDate) >= 0)
+            {
+                BaseTestSite.Assert.Inconclusive("The object store SHOULD set Open.File.LastModificationTime to the current system time.");
+            }
+            else if (oldLastAccessTimeDate.CompareTo(lastAccessTimeDate) >= 0)
+            {
+                BaseTestSite.Assert.Pass("The object store SHOULD set Open.File.LastModificationTime to the current system time.");
+                BaseTestSite.Assert.Inconclusive("The object store SHOULD set Open.File.LastAccessTime to the current system time.");
+            }
+            else if (oldChangeTimeDate.CompareTo(changeTimeDate) >= 0)
+            {
+                BaseTestSite.Assert.Pass("The object store SHOULD set Open.File.LastModificationTime to the current system time.");
+                BaseTestSite.Assert.Pass("The object store SHOULD set Open.File.LastAccessTime to the current system time.");
+                BaseTestSite.Assert.Inconclusive("The object store SHOULD set Open.File.LastChangeTime to the current system time.");
+            }
+            else if (isArchive == false)
+            {
+                BaseTestSite.Assert.Pass("The object store SHOULD set Open.File.LastModificationTime to the current system time.");
+                BaseTestSite.Assert.Pass("The object store SHOULD set Open.File.LastAccessTime to the current system time.");
+                BaseTestSite.Assert.Pass("The object store SHOULD set Open.File.LastChangeTime to the current system time.");
+
+                //Verify FILE_ATTRIBUTE_ARCHIVE is set to TRUE
+                BaseTestSite.Assert.Inconclusive("The object store SHOULD set Open.File.FileAttributes.FILE_ATTRIBUTE_ARCHIVE to TRUE.");
+            }
+            else
+            {
+                BaseTestSite.Assert.Pass("The object store SHOULD set Open.File.LastModificationTime to the current system time.");
+                BaseTestSite.Assert.Pass("The object store SHOULD set Open.File.LastChangeTime to the current system time.");
+                BaseTestSite.Assert.Pass("The object store SHOULD set Open.File.LastAccessTime to the current system time.");
+                BaseTestSite.Assert.Pass("The object store SHOULD set Open.File.FileAttributes.FILE_ATTRIBUTE_ARCHIVE to TRUE.");
+            }
         }
 
-        private void CreateFile(FileType fileType)
+        private void OpenFile(string fileName)
         {
-            string fileName = this.fsaAdapter.ComposeRandomFileName(8);
+            //open file after a time interval
+            DateTime currentTime = DateTime.Now;
+            DateTime nextTime = DateTime.Now;
+
+            while (currentTime.ToString().Equals(nextTime.ToString()))
+            {
+                nextTime = DateTime.Now;
+            }
+
+            MessageStatus status = this.fsaAdapter.CreateFile(
+                fileName,
+                FileAttribute.NORMAL,
+                CreateOptions.NON_DIRECTORY_FILE,
+                FileAccess.GENERIC_READ | FileAccess.GENERIC_WRITE | FileAccess.FILE_WRITE_DATA | FileAccess.FILE_WRITE_ATTRIBUTES,
+                ShareAccess.FILE_SHARE_READ | ShareAccess.FILE_SHARE_WRITE,
+                CreateDisposition.OPEN);
+
+            BaseTestSite.Assert.AreEqual(MessageStatus.SUCCESS, status, "Open should succeed.");
+        }
+
+        private void CreateFile(FileType fileType, out string fileName)
+        {
+            fileName = this.fsaAdapter.ComposeRandomFileName(8);
             MessageStatus status = this.fsaAdapter.CreateFile(
                 fileName,
                 FileAttribute.NORMAL,
