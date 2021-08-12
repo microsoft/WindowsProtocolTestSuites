c668940e98c266dd90579ff5dd8f3ae5662c201b
Zheyu Shen <zheyushen@microsoft.com>
2021-08-12 18:01:28 +0800

    add react-devtools

--- ProtocolTestManager/PTMService/PTMService/ClientApp/src/index.tsx
+++ ProtocolTestManager/PTMService/PTMService/ClientApp/src/index.tsx
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+import 'react-devtools';
 import { initializeIcons } from '@uifabric/icons';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
