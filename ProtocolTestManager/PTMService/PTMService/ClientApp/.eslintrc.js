39129ead3185ad3d87673c340bb849e7d2bd6e97
Zheyu Shen <zheyushen@microsoft.com>
2021-08-13 11:14:16 +0800

    Add ESLint (warning only)

---
+++ ProtocolTestManager/PTMService/PTMService/ClientApp/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'only-warn',
+  ],
+  rules: {
+  },
+};
