e507dcfa5016a88366f806590f4a0ebdf2738834
Zheyu Shen <zheyushen@microsoft.com>
2021-08-18 17:56:42 +0800

    Factor out PopupModal

--- ProtocolTestManager/PTMService/PTMService/ClientApp/src/pages/AutoDetection.tsx
+++ ProtocolTestManager/PTMService/PTMService/ClientApp/src/pages/AutoDetection.tsx
@@ -21,6 +21,7 @@ import {
   TooltipHost
 } from '@fluentui/react'
 import { StepWizardChildProps, StepWizardProps } from 'react-step-wizard'
+import { PopupModal } from '../components/PopupModal'
 import { StepPanel } from '../components/StepPanel'
 import { WizardNavBar } from '../components/WizardNavBar'
 import { getNavSteps } from '../model/DefaultNavSteps'
@@ -362,85 +363,8 @@ export function AutoDetection (props: StepWizardProps) {
                 </Stack>
             </StepPanel>
 
-            <Modal
-                isOpen={isAutoDetectionWarningDialogOpen}
-                containerClassName={contentStyles.container}
-            >
-              <div className={contentStyles.header}>
-                <span>Warning</span>
-                <IconButton
-                  styles={iconButtonStyles}
-                  iconProps={cancelIcon}
-                  onClick={hideAutoDetectionWarningDialog}
-                />
-              </div>
-              <div className={contentStyles.body}>
-                  <TextField multiline readOnly autoAdjustHeight defaultValue={autoDetectionStepsResult?.Exception} />
-              </div>
-            </Modal>
-
-            <Modal
-                isOpen={isAutoDetectionLogDialogOpen}
-                containerClassName={contentStyles.container}
-            >
-              <div className={contentStyles.header}>
-                <span>Log</span>
-                <IconButton
-                  styles={iconButtonStyles}
-                  iconProps={cancelIcon}
-                  onClick={hideAutoDetectionLogDialog}
-                />
-              </div>
-              <div className={contentStyles.body}>
-                  <TextField multiline readOnly autoAdjustHeight defaultValue={autoDetectionLog} />
-              </div>
-            </Modal>
+            <PopupModal isOpen={isAutoDetectionWarningDialogOpen} header={'Warning'} onClose={hideAutoDetectionWarningDialog} text={autoDetectionStepsResult?.Exception} />
+            <PopupModal isOpen={isAutoDetectionLogDialogOpen} header={'Log'} onClose={hideAutoDetectionLogDialog} text={autoDetectionLog} />
         </div>
   )
 };
-
-const theme = getTheme()
-const contentStyles = mergeStyleSets({
-  container: {
-    display: 'flex',
-    flexFlow: 'column nowrap',
-    alignItems: 'stretch',
-    minWidth: '50%'
-  },
-  header: [
-    theme.fonts.xxLarge,
-    {
-      flex: '1 1 auto',
-      borderTop: `4px solid ${theme.palette.themePrimary}`,
-      color: theme.palette.neutralPrimary,
-      display: 'flex',
-      alignItems: 'center',
-      fontWeight: FontWeights.semibold,
-      padding: '12px 12px 14px 24px'
-    }
-  ],
-  body: {
-    flex: '4 4 auto',
-    padding: '0 24px 24px 24px',
-    overflowY: 'hidden',
-    selectors: {
-      p: { margin: '14px 0' },
-      'p:first-child': { marginTop: 0 },
-      'p:last-child': { marginBottom: 0 }
-    }
-  }
-})
-
-const cancelIcon: IIconProps = { iconName: 'Cancel' }
-
-const iconButtonStyles: Partial<IButtonStyles> = {
-  root: {
-    color: theme.palette.neutralPrimary,
-    marginLeft: 'auto',
-    marginTop: '4px',
-    marginRight: '2px'
-  },
-  rootHovered: {
-    color: theme.palette.neutralDark
-  }
-}
