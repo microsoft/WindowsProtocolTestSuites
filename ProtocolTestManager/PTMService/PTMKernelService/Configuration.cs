7a83e3e5d9f2c103a93120c6b0d26a279109f5de
Kyle Zhang <v-zhanh@microsoft.com>
2021-09-02 15:19:49 +0800

    copy adapters to ptf folder

--- ProtocolTestManager/PTMService/PTMKernelService/Configuration.cs
+++ ProtocolTestManager/PTMService/PTMKernelService/Configuration.cs
@@ -418,9 +418,65 @@ namespace Microsoft.Protocols.TestManager.PTMService.PTMKernelService
                 File.Copy(file, Path.Combine(ptfConfigStorage.AbsolutePath, name));
             }
 
+            var ptfConfig = new PtfConfig(ptfConfigStorage.GetFiles().ToList());
+            var scriptAdapters = ptfConfig.adapterTable.Select(kvp =>
+            {
+                var node = kvp.Value;
+                string type = node.Attributes[ConfigurationConsts.AdapterKindAttributeName].Value;
+                if (node.Attributes[ConfigurationConsts.AdapterScriptDirectoryAttributeName] != null &&
+                !string.IsNullOrEmpty(node.Attributes[ConfigurationConsts.AdapterScriptDirectoryAttributeName].Value))
+                {
+                    return node.Attributes[ConfigurationConsts.AdapterScriptDirectoryAttributeName].Value;
+                }
+                return null;                
+            }).Where(x => x != null).ToList();
+           
+            foreach (string dir in scriptAdapters)
+            {
+                var source = Path.Combine(testSuite.StorageRoot.GetNode(TestSuiteConsts.Bin).AbsolutePath, dir);
+                var target = Path.Combine(ptfConfigStorage.AbsolutePath, dir);
+                DirectoryCopy(source, target, true);
+            }
+
             var result = new Configuration(testSuiteConfiguration, testSuite, configurationNode);
 
             return result;
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // If the destination directory doesn't exist, create it.       
+            Directory.CreateDirectory(destDirName);
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(tempPath, true);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string tempPath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+                }
+            }
         }
 
         public static Configuration Open(TestSuiteConfiguration testSuiteConfiguration, ITestSuite testSuite, IStoragePool storagePool)
