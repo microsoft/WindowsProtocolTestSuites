38d5eaae4df9d08da3625b4d228ed7d3ac3503e4
Xianbin Zhang <v-xiazhang@microsoft.com>
2021-10-21 16:11:20 +0800

    add ps

---
+++ TestSuites/ADFamily/src/Batch/RunDRSRCases_Win8.1.ps1
@@ -0,0 +1,16 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+param(
+    [switch]$DryRun = $false # If set, just list all test cases instead of running tests actually.
+)
+
+$invocationPath =  Split-Path $MyInvocation.MyCommand.Definition -Parent
+
+$script = Join-Path $invocationPath "RunTestCasesByFilter.ps1"
+
+$Filter="TestCategory=MS-DRSR&TestCategory!=WinThreshold&TestCategory!=Winv1803"
+
+$cmd = "$script -Filter `$Filter -DryRun:`$(`$DryRun.IsPresent)"
+
+Invoke-Expression $cmd
