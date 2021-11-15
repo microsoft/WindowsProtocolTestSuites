07dbab8c384ce4737839e55ca45e0ffb8b9a5d41
Kyle Zhang <v-zhanh@microsoft.com>
2021-11-15 09:12:23 +0000

    Merged PR 6650960: [dotnet-fx] [Kerberos]Fix test case failures related to Kerberos changes.
    Kerberos cksumtype issue
    
    Related work items: #36975333

--- ProtoSDK/KerberosLib/Asn1Code/Rfc4120/Int32.cs
+++ ProtoSDK/KerberosLib/Asn1Code/Rfc4120/Int32.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Microsoft.Protocols.TestTools.StackSdk.Asn1;
+using System;
 
 namespace Microsoft.Protocols.TestTools.StackSdk.Security.KerberosLib
 {
@@ -12,18 +13,68 @@ namespace Microsoft.Protocols.TestTools.StackSdk.Security.KerberosLib
     {
         protected override bool VerifyConstraints()
         {
-            return this.Value >=-2147483648 && this.Value <= 2147483647;
+            return this.Value >= -2147483648 && this.Value <= 2147483647;
         }
 
         public KerbInt32()
             : base()
         {
         }
-        
+
         public KerbInt32(long? val)
             : base(val)
         {
         }
+
+        private static int BytesCountForLong { get { return sizeof(long); } }
+
+        protected override int ValueBerDecode(IAsn1DecodingBuffer buffer, int length)
+        {
+            byte[] result = buffer.ReadBytes(length);
+            Value = IntegerDecoding(result);
+            return length;
+        }
+
+        private static long IntegerDecoding(byte[] encodingResult)
+        {
+            int len = encodingResult.Length;
+            if (len == 0)
+            {
+                throw new Asn1DecodingOutOfBufferRangeException(ExceptionMessages.DecodingOutOfRange);
+            }
+            byte[] fullContent = GetExpandedContent(encodingResult);//fullContent is little endian
+            if (!BitConverter.IsLittleEndian)
+            {
+                //If the system is not little endian, then change fullcontent to big endian.
+                Array.Reverse(fullContent);
+            }
+            return BitConverter.ToInt64(fullContent, 0);
+        }
+
+        private static byte[] GetExpandedContent(byte[] bigEndianEncodingResult)
+        {
+            byte[] fullContent = new byte[BytesCountForLong];
+            //adjust to little endian and expand the array so that it could be processed by BitConverter.
+            Array.Copy(bigEndianEncodingResult, fullContent, bigEndianEncodingResult.Length);
+            Array.Reverse(fullContent, 0, bigEndianEncodingResult.Length);
+            int curPos = bigEndianEncodingResult.Length;
+
+            if (bigEndianEncodingResult[0] < 128) //it's a positive data's encoding result
+            {
+                while (curPos != sizeof(long))
+                {
+                    fullContent[curPos++] = 0;
+                }
+            }
+            else//it's a negative data's encoding result
+            {
+                while (curPos != BytesCountForLong)
+                {
+                    fullContent[curPos++] = 255;
+                }
+            }
+
+            return fullContent;
+        }
     }
 }
-
