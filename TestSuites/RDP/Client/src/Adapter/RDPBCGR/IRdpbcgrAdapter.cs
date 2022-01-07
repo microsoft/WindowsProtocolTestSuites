7b8e0483dc40d3b996cca8a9590ddfee789466fb
James Omitiran <jomitiran@microsoft.com>
2022-01-08 00:19:44 +0800

    Change Interface

--- TestSuites/RDP/Client/src/Adapter/RDPBCGR/IRdpbcgrAdapter.cs
+++ TestSuites/RDP/Client/src/Adapter/RDPBCGR/IRdpbcgrAdapter.cs
@@ -176,13 +176,6 @@ namespace Microsoft.Protocols.TestSuites.Rdpbcgr
         void ExpectTransportConnection(RDPSessionType sessionType);
 
         /// <summary>
-        /// Expect a transport level (TCP) non connection
-        /// </summary>
-        /// <param name="sessionType">The type of session to be established.</param>
-        /// <param name="invalidCredentials">Invalid Credentials used.</param>
-        void ExpectTransportConnection(RDPSessionType sessionType, bool invalidCredentials);
-
-        /// <summary>
         /// Send Server X_224 Connection Confirm Pdu to client
         /// </summary>
         /// <param name="protocol">Indicates the selected security protocol</param>
