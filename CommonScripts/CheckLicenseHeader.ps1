e3fd96eec6d480a41af9352f596ba6f69ae13d16
Minghao Hu <v-minghu@microsoft.com>
2022-01-13 06:27:00 +0000

    Merged PR 6837124: [GitHub] Add new logics to fetch GitHub PR branch.
    Related work items: #37721338

--- CommonScripts/CheckLicenseHeader.ps1
+++ CommonScripts/CheckLicenseHeader.ps1
@@ -3,30 +3,43 @@
 param (
     [string]$TestSuitePath,
     [string]$IsCheckAll = "false",
-    [string]$targetBranch = "staging",
-    [string]$sourceBranch
+    [string]$targetBranch = "main",
+    [string]$sourceBranch,
+    [string]$prId
 )
 
-$InvocationPath = Split-Path $MyInvocation.MyCommand.Definition -parent
-if($targetBranch -notmatch "refs/heads/(.+)")
-{
+$InvocationPath = Split-Path $MyInvocation.MyCommand.Definition -Parent
+
+if ($targetBranch -notmatch "refs/heads/(.+)") {
     $targetBranch = "origin/$targetBranch"
 }
-if($sourceBranch -notmatch "refs/heads/(.+)")
-{
-    $sourceBranch = "origin/$sourceBranch"
+
+$isGitHubPR = $false
+if ($sourceBranch -notmatch "refs/heads/(.+)") {
+    $sourceBranch = "origin/pull/$prId"
+    $isGitHubPR = $true
 }
 
 Write-Host "InvocationPath: $InvocationPath"
-Write-Host "TestSuitePath:$TestSuitePath"
-Write-Host "targetBranch:$targetBranch"
-Write-Host "sourceBranch:$sourceBranch"
+Write-Host "TestSuitePath: $TestSuitePath"
+Write-Host "targetBranch: $targetBranch"
+Write-Host "sourceBranch: $sourceBranch"
+if ($isGitHubPR) {
+    Write-Host "prId: $prId"
+}
 
 Push-Location $TestSuitePath
+
+if ($isGitHubPR) {
+    Write-Host "Fetch the GitHub PR branch"
+    git fetch origin "pull/$prId/head:$sourceBranch"
+}
+
 $Diff = git diff --name-only "$targetBranch...$sourceBranch"
+
 Pop-Location
 
-$extension = ".ps1",".cs",".bat",".cmd",".reg",".sh",".psm1"
+$extension = ".ps1", ".cs", ".bat", ".cmd", ".reg", ".sh", ".psm1"
 $result = New-Object 'System.Collections.Generic.List[String]'
 
 function Verify-License {
@@ -38,53 +51,52 @@ function Verify-License {
     $license = ""
     switch ($filename.Substring($filename.LastIndexOf('.'))) {        
         ".cs" {
-            $license = `
+            $license = 
             "// Copyright (c) Microsoft. All rights reserved.`r`n" + 
             "// Licensed under the MIT license. See LICENSE file in the project root for full license information."; break
         }
         ".reg" {
-            $license = `
+            $license = 
             "; Copyright (c) Microsoft. All rights reserved.`r`n" + 
             "; Licensed under the MIT license. See LICENSE file in the project root for full license information."; break
         }
-        {$_ -eq ".cmd" -or ($_ -eq ".bat")} {
-            $license = `
+        { $_ -eq ".cmd" -or ($_ -eq ".bat") } {
+            $license = 
             ":: Copyright (c) Microsoft. All rights reserved.`r`n" + 
             ":: Licensed under the MIT license. See LICENSE file in the project root for full license information."; break
         }
-        {$_ -eq ".ps1" -or ($_ -eq ".psm1")} {
-            $license = `
+        { $_ -eq ".ps1" -or ($_ -eq ".psm1") } {
+            $license = 
             "# Copyright (c) Microsoft. All rights reserved.`r`n" + 
             "# Licensed under the MIT license. See LICENSE file in the project root for full license information."; break
         }
         ".sh" {
-            $license = `
+            $license = 
             "# Copyright (c) Microsoft. All rights reserved.`n" + 
             "# Licensed under the MIT license. See LICENSE file in the project root for full license information."; break
         }
     }
 
-    if(!$content.Contains($license)){
+    if (!$content.Contains($license)) {
         $result.Add($filename)
     }   
 }
 
-if($IsCheckAll -eq "true"){
+if ($IsCheckAll -eq "true") {
     Write-Host "Check all files license header"
-    Get-ChildItem $TestSuitePath  -Exclude "_Helper" -Recurse | ForEach-Object{ `
-        if($extension.Contains($_.Extension.ToLower())){
+    Get-ChildItem $TestSuitePath  -Exclude "_Helper" -Recurse | ForEach-Object { `
+            if ($extension.Contains($_.Extension.ToLower())) {
             Write-Host "Check file: $_"
             $content = Get-Content $_.FullName -raw
             Verify-License -filename $_ -content $content
         }
-  }
+    }
 }
 else {
     Write-Host "Check the license header for the different files"    
-    $Diff | ForEach-Object{
+    $Diff | ForEach-Object {
         $file = $_.Trim()
-        if($extension.Contains($file.SubString($file.lastindexOf('.')).Trim().ToLower()) -and (Test-Path "$TestSuitePath\$file"))
-        {
+        if ($extension.Contains($file.SubString($file.lastindexOf('.')).Trim().ToLower()) -and (Test-Path "$TestSuitePath\$file")) {
             Write-Host "Check file: $TestSuitePath\$file"   
             $content = Get-Content "$TestSuitePath\$file" -raw
             Verify-License -filename $file -content $content
@@ -92,7 +104,7 @@ else {
     }
 }
 
-if ($result.Count -gt 0){
+if ($result.Count -gt 0) {
     Write-Host "==========================================================="
     Write-Host "Documents found not in compliance with license requirements"
     $result
