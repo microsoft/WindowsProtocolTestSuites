1a3556f152531a43a26d48b8344ef3dbaab488fd
James Omitiran <jomitiran@microsoft.com>
2022-05-04 15:14:29 -0700

    Adjustments to pointer change

--- TestSuites/RDP/RDPSUTControlAgent/CSharp/RDPSUTControlAgent/Server/PointerManager.cs
+++ TestSuites/RDP/RDPSUTControlAgent/CSharp/RDPSUTControlAgent/Server/PointerManager.cs
@@ -5,22 +5,16 @@ namespace RDPSUTControlAgent
 {
     public static class PointerManager
     {
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        private static extern Int32 SystemParametersInfo(UInt32 uiAction, UInt32 uiParam, String pvParam, UInt32 fWinIni);
-        private static readonly uint SPI_SETCURSORS = 87;
-        private static readonly uint SPIF_UPDATEINIFILE = 0x1;
-        private static readonly uint SPIF_SENDCHANGE = 0x2;
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+        public static extern bool SystemParametersInfo(uint uiAction, uint uiParam, uint pvParam, uint fWinIni);
+        private static readonly uint SPI_SETCURSORS = 0x0057;
+        private static readonly uint SPIF_UPDATEINIFILE = 0x01;
+        private static readonly uint SPIF_SENDCHANGE = 0x02;
 
         public static void EffectChange()
         {
-            SystemParametersInfo(SPI_SETCURSORS, 0, null, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+            SystemParametersInfo(SPI_SETCURSORS, 0, 0, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
         }
     }
 
-    public enum SPIF
-    {
-        None = 0x00,
-        UPDATEINIFILE = 0x01,
-        SENDCHANGE = 0x02
-    }
 }
--- TestSuites/RDP/RDPSUTControlAgent/CSharp/RDPSUTControlAgent/Server/RDPServerControl.cs
+++ TestSuites/RDP/RDPSUTControlAgent/CSharp/RDPSUTControlAgent/Server/RDPServerControl.cs
@@ -42,7 +42,11 @@ namespace RDPSUTControlAgent
                         key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Cursors",true);
                         if (key != null)
                         {
-                            key.SetValue("", "Windows Standard (extra large)");
+                            foreach(var registryValue in ExtraLargeDefaultPointerRegistrySet())
+                            {
+                                key.SetValue(registryValue.Key, registryValue.Value);
+                            }
+                            
                             key.Close();
                         }
 
@@ -56,7 +60,11 @@ namespace RDPSUTControlAgent
                         key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Cursors", true);
                         if (key != null)
                         {
-                            key.SetValue("", "Windows Standard");
+                            foreach (var registryValue in DefaultPointerRegistrySet())
+                            {
+                                key.SetValue(registryValue.Key, registryValue.Value);
+                            }
+
                             key.Close();
                         }
 
@@ -80,5 +88,52 @@ namespace RDPSUTControlAgent
             return responseMessage;
         }
 
+        private Dictionary<string, string> DefaultPointerRegistrySet()
+        {
+            Dictionary<string, string> pointerRegistrySet = new Dictionary<string, string>();
+
+            pointerRegistrySet[""] = "Windows Default";
+            pointerRegistrySet["AppStarting"] = "%SystemRoot%\\cursors\\aero_working.ani";
+            pointerRegistrySet["Arrow"] = "%SystemRoot%\\cursors\\aero_arrow.cur";
+            pointerRegistrySet["Hand"] = "%SystemRoot%\\cursors\\aero_link.cur";
+            pointerRegistrySet["Help"] = "%SystemRoot%\\cursors\\aero_helpsel.cur";
+            pointerRegistrySet["No"] = "%SystemRoot%\\cursors\\aero_unavail.cur";
+            pointerRegistrySet["NWPen"] = "%SystemRoot%\\cursors\\aero_pen.cur";
+            pointerRegistrySet["Person"] = "%SystemRoot%\\cursors\\aero_person.cur";
+            pointerRegistrySet["Pin"] = "%SystemRoot%\\cursors\\aero_pin.cur";
+            pointerRegistrySet["SizeAll"] = "%SystemRoot%\\cursors\\aero_move.cur";
+            pointerRegistrySet["SizeNESW"] = "%SystemRoot%\\cursors\\aero_nesw.cur";
+            pointerRegistrySet["SizeNS"] = "%SystemRoot%\\cursors\\aero_ns.cur";
+            pointerRegistrySet["SizeNWSE"] = "%SystemRoot%\\cursors\\aero_nwse.cur";
+            pointerRegistrySet["SizeWE"] = "%SystemRoot%\\cursors\\aero_ew.cur";
+            pointerRegistrySet["UpArrow"] = "%SystemRoot%\\cursors\\aero_up.cur";
+            pointerRegistrySet["Wait"] = "%SystemRoot%\\cursors\\aero_busy.ani";
+
+            return pointerRegistrySet;
+        }
+
+        private Dictionary<string, string> ExtraLargeDefaultPointerRegistrySet()
+        {
+            Dictionary<string, string> pointerRegistrySet = new Dictionary<string, string>();
+
+            pointerRegistrySet[""] = "Windows Default (extra large)";
+            pointerRegistrySet["AppStarting"] = "%SystemRoot%\\cursors\\aero_working_xl.ani";
+            pointerRegistrySet["Arrow"] = "%SystemRoot%\\cursors\\aero_arrow_xl.cur";
+            pointerRegistrySet["Hand"] = "%SystemRoot%\\cursors\\aero_link_xl.cur";
+            pointerRegistrySet["Help"] = "%SystemRoot%\\cursors\\aero_helpsel_xl.cur";
+            pointerRegistrySet["No"] = "%SystemRoot%\\cursors\\aero_unavail_xl.cur";
+            pointerRegistrySet["NWPen"] = "%SystemRoot%\\cursors\\aero_pen_xl.cur";
+            pointerRegistrySet["Person"] = "%SystemRoot%\\cursors\\aero_person_xl.cur";
+            pointerRegistrySet["Pin"] = "%SystemRoot%\\cursors\\aero_pin_xl.cur";
+            pointerRegistrySet["SizeAll"] = "%SystemRoot%\\cursors\\aero_move_xl.cur";
+            pointerRegistrySet["SizeNESW"] = "%SystemRoot%\\cursors\\aero_nesw_xl.cur";
+            pointerRegistrySet["SizeNS"] = "%SystemRoot%\\cursors\\aero_ns_xl.cur";
+            pointerRegistrySet["SizeNWSE"] = "%SystemRoot%\\cursors\\aero_nwse_xl.cur";
+            pointerRegistrySet["SizeWE"] = "%SystemRoot%\\cursors\\aero_ew_xl.cur";
+            pointerRegistrySet["UpArrow"] = "%SystemRoot%\\cursors\\aero_up_xl.cur";
+            pointerRegistrySet["Wait"] = "%SystemRoot%\\cursors\\aero_busy_xl.ani";
+
+            return pointerRegistrySet;
+        }
     }
 }
