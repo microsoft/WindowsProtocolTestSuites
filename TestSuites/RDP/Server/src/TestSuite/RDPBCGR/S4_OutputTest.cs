ac97c9b92b0b2d5d0f2735d8b5ea11e06e923785
James Omitiran <jomitiran@microsoft.com>
2022-05-05 13:21:47 +0100

    Add test to correct file

--- TestSuites/RDP/Server/src/TestSuite/RDPBCGR/S4_OutputTest.cs
+++ TestSuites/RDP/Server/src/TestSuite/RDPBCGR/S4_OutputTest.cs
@@ -63,5 +63,43 @@ namespace Microsoft.Protocols.TestSuites.Rdpbcgr
 
             #endregion Test Code
         }
+
+        [TestMethod]
+        [Priority(1)]
+        [TestCategory("BVT")]
+        [TestCategory("RDP7.0")]
+        [TestCategory("RDPBCGR")]
+        [Description(@"This test case is used to verify the message sent to the driver by SUT when there is a change to the pointer size.")]
+        public void S4_Output_PointerSize_FastPathOutput()
+        {
+            #region Test Steps
+            //1. Initiate an RDP connection to RDP server (SUT) and complete the Connection Initiation phase, Basic Setting Exchange phase, and Channel Connection phase.
+            //2. Adjust pointer size
+            //3. Expect PDU showing the pointer size has been changed.
+            #endregion Test Steps
+
+            #region Test Code
+
+            this.Site.Log.Add(LogEntryKind.Comment, "Establish transport connection with RDP Server, encrypted protocol is {0}.", testConfig.transportProtocol.ToString());
+            rdpbcgrAdapter.ConnectToServer(testConfig.transportProtocol);
+
+            string[] SVCNames = new string[] { RdpConstValue.SVCNAME_RDPEDYC };
+            rdpbcgrAdapter.EstablishRDPConnection(testConfig.requestProtocol, SVCNames, CompressionType.PACKET_COMPR_TYPE_RDP61,
+                false, // Is reconnect
+                true,  // Is auto logon
+                supportFastPathInput: true,
+                supportFastPathOutput: true);
+
+            this.Site.Log.Add(LogEntryKind.Comment, "Increase pointer size.");
+            PointerIncreaseSize();
+            //Thread.Sleep(5000);
+
+            this.Site.Log.Add(LogEntryKind.Comment, "Wait a period to receive and verify all fast-path update PDUs received ");
+            rdpbcgrAdapter.ExpectPointerAttributeOutputs(testConfig.timeout);
+
+            this.Site.Log.Add(LogEntryKind.Comment, "Reverse to pointer default size.");
+            PointerReverseToDefaultSize();
+            #endregion Test Code
+        }
     }
 }
