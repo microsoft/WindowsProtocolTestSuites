b44ac1bae05ea0970718567ac30fbab99237d8df
Victor Ding <shoding@microsoft.com>
2022-06-01 21:31:12 +0800

    upgrade version to 4.22.6.0

--- AssemblyInfo/PTMAssemblyInfo.cs
+++ AssemblyInfo/PTMAssemblyInfo.cs
@@ -5,4 +5,4 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Windows Protocol Test Suites")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
