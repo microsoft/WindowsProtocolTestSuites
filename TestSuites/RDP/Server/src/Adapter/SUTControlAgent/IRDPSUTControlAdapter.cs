152a9d8834e6f9154254e6c0425ad02e66dafe80
James Omitiran <jomitiran@microsoft.com>
2022-07-27 12:35:45 +0100

    Add copyright to page

--- TestSuites/RDP/Server/src/Adapter/SUTControlAgent/IRDPSUTControlAdapter.cs
+++ TestSuites/RDP/Server/src/Adapter/SUTControlAgent/IRDPSUTControlAdapter.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Protocols.TestTools;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Protocols.TestTools;
 
 namespace Microsoft.Protocols.TestSuites.Rdp
 {
