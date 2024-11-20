13c00552abc2c23865265f2f789d7a674fdf15b0
Will Wang <v-wangwil@microsoft.com>
2024-11-20 02:39:43 +0000

    Updated TriggerResolutionChangeOnClient.ps1

--- TestSuites/RDP/Client/src/Adapter/RdpedispSUTControlAdapter/TriggerResolutionChangeOnClient.ps1
+++ TestSuites/RDP/Client/src/Adapter/RdpedispSUTControlAdapter/TriggerResolutionChangeOnClient.ps1
@@ -5,6 +5,18 @@
 # Return Value: 0 indicates task is started successfully; -1 indicates failed to run the specified task
 
 # Run Task to change remote screen orientation
+$cmdOutput = ""
+$userPwdInTCEn = ConvertTo-SecureString $ptfprop_SUTUserPassword -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential($ptfprop_SUTUserName,$userPwdInTCEn)
+$sessionM = $null;
+try
+{
+	$sessionM = New-PSSession -ComputerName $ptfprop_SUTName -Credential $Credential
+}
+catch
+{
+}
+
 $path = "/RDP-TestSuite-ClientEP/Scripts"
 $scriptblock = {
 	param([int]$width, [int]$height, [string]$path)
@@ -18,9 +30,19 @@ $scriptblock = {
 	
 $cmdOutput = Invoke-Command -HostName $ptfprop_SUTName -UserName $ptfprop_SUTUserName -ScriptBlock $scriptblock -ArgumentList ($width, $height, $path)
 
+$cmdOutput = Invoke-Command -HostName $ptfprop_SUTName -UserName $ptfprop_SUTUserName -ScriptBlock $scriptblock -ArgumentList ($width, $height, $path)
+if ($null -eq $sessionM)
+{
+    $cmdOutput = Invoke-Command -HostName $ptfprop_SUTName -UserName $ptfprop_SUTUserName -ScriptBlock $scriptblock -ArgumentList ($width, $height, $path)
+}
+else
+{
+    $cmdOutput = Invoke-Command -Session $sessionM -ScriptBlock $scriptblock -ArgumentList ($width, $height, $path)
+}
+
 $cmdOutput | out-file "./ChangeResolution.log"
 
-if($cmdOutput -ne $null)
+if($null -ne $cmdOutput)
 {
     if($cmdOutput.GetType().IsArray)
     {
