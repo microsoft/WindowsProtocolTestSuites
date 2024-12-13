34439f0f001b3a737920f20003477016dc8cc114
Will Wang (Shanghai Wicresoft Co Ltd) <v-wangwil@microsoft.com>
2024-12-13 05:55:23 +0000

    Updated Config-DriverComputer.ps1

--- TestSuites/RDP/Server/Setup/Scripts/Config-DriverComputer.ps1
+++ TestSuites/RDP/Server/Setup/Scripts/Config-DriverComputer.ps1
@@ -64,6 +64,9 @@ if ($SutOSVersion -ge 10.0) {
     if ($SutOsBuildNumber -ge 26100) {
         $RDPVersion = "10.12"
     }
+    elseif ($SutOsBuildNumber -ge 20000) {
+        $RDPVersion = "10.9"
+    }
     elseif ($SutOsBuildNumber -ge 19041) {
         $RDPVersion = "10.8"
     }
